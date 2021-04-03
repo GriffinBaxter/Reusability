a0ae830dbe11dc81003d647d1818669bb2c1acdb
Billie Johnson <baj40@uclive.ac.nz>
2021-04-04 01:58:40 +1300

    S302T400-37 #time 10m Implemented function in ProductRepository interface to find a product with a given produt and business id ?implement ?document
    - Added findProductByIdAndBusinessId in ProductRepository

--- backend/src/main/java/org/seng302/business/ProductRepository.java
+++ backend/src/main/java/org/seng302/business/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * ProductRepository interface
@@ -18,5 +19,14 @@ public interface ProductRepository extends JpaRepository<Product, String> {
      * @return A list of products with the given business ID.
      */
     List<ProductPayload> findProductsByBusinessId(Integer businessId);
+
+    /**
+     * Finds a product with the given product ID and business ID if one exists.
+     *
+     * @param id A product ID
+     * @param businessId A business ID
+     * @return A product with the given product ID and business ID.
+     */
+    Optional<Product> findProductByIdAndBusinessId(String id, Integer businessId);
 
 }
