a8d4e9f25b8e6b006e0adc7740e7f9736cf45cfb
Jack Patterson <jlp89@uclive.ac.nz>
2021-04-08 11:07:34 +1200

    S302T400-53 #time 1h Searching now allows for all name combinations ?implement ?pair[jlp89, hek21] ?testmanual
    Manually tested using front-end:
    Tested each name alone,
    firstname + middlename,
    firstname + lastname,
    firstname + middlename + lastname,
    middlename + lastname,
    nickname
    as search combinations

--- backend/src/main/java/org/seng302/user/UserResource.java
+++ backend/src/main/java/org/seng302/user/UserResource.java
@@ -232,7 +232,9 @@ public class UserResource {
 
         Pageable paging = PageRequest.of(pageNo, pageSize, sortBy);
         // TODO Allow searching by more than one name at once
-        Page<User> pagedResult = userRepository.findAllByFirstNameContainsIgnoreCaseOrMiddleNameContainsIgnoreCaseOrLastNameContainsIgnoreCaseOrNicknameContainsIgnoreCase(searchQuery, searchQuery, searchQuery, searchQuery, paging);
+        Page<User> pagedResult = userRepository.findAllUsersByNames(searchQuery, paging);
+        //Page<User> pagedResult = userRepository.findAllByFirstNameContainsIgnoreCaseOrMiddleNameContainsIgnoreCaseOrLastNameContainsIgnoreCaseOrNicknameContainsIgnoreCase(searchQuery, searchQuery, searchQuery, searchQuery, paging);
+
         int totalPages = pagedResult.getTotalPages();
         int totalRows = (int) pagedResult.getTotalElements();
 
