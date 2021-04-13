3168f77b63d6f8859c0d8918f0ca5896c395799c
Hayley Krippner <hek21@uclive.ac.nz>
2021-04-13 23:58:56 +1200

    S302T400-161 #time 3m Checking ProfileHeader.vue's comments, adding additional comments and sectioning sections of code to increase readability. ?document

--- frontend/src/components/ProfileHeader.vue
+++ frontend/src/components/ProfileHeader.vue
@@ -1,3 +1,6 @@
+<!--This file creates the profile header for the Profile page.-->
+<!--It contains the search bar for searching for users by first name.-->
+
 <template>
     <div class="pt-3 text-font" id=" search-bar-container">
       <div class="row text-center">
@@ -16,6 +19,10 @@
 export default {
   name: "ProfileHeader",
   methods: {
+
+    /**
+     * When the enter key is pressed, the query is run with the search value and the user is routed to the search page.
+     */
     enterPressed(event) {
       if (event.keyCode === 13) {
         // Enter pressed
@@ -23,6 +30,10 @@ export default {
         this.$router.push({ path: '/search', query: { searchQuery: `${inputQuery}` }})
       }
     },
+
+    /**
+     * When the search button is clicked, the query is run with the search value and the user is routed to the search page.
+     */
     searchClicked() {
       const inputQuery = this.$refs.searchInput.value;
       this.$router.push({ path: '/search', query: { searchQuery: `${inputQuery}` }})
@@ -31,17 +42,19 @@ export default {
 }
 </script>
 
+<!-------------------------------------------- Navigation Bar Styling ------------------------------------------------->
+
 <style scoped>
 
 .search-bar-positioning {
   padding-top: 40px;
 }
 
+/*TODO consider putting in style.css*/
 #search-bar:focus {
   outline: none;     /* oranges! yey */
   box-shadow: 0 0 2px 2px #2eda77; /* Full freedom. (works also with border-radius) */
   border: 1px solid #1EBABC;
 }
-
 
 </style>
