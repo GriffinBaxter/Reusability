21e13f3b79ba8392643e6aeee2fb75d9172addfc
Hayley Krippner <hek21@uclive.ac.nz>
2021-04-14 01:28:36 +1200

    S302T400-161 #time 1m Checking ServerTimeout.vue's comments, adding additional comments, refactoring id and class names to being lowercase and hyphenated, and sectioning sections of code to increase readability. ?document

--- frontend/src/views/ServerTimeout.vue
+++ frontend/src/views/ServerTimeout.vue
@@ -1,3 +1,5 @@
+<!--This file creates the page that the user is taken to when the "Server timeout" error occurs-->
+
 <template>
   <div class="container py-4 mt-4 shadow text-font">
     Server timeout.
@@ -9,6 +11,8 @@ export default {
   name: "ServerTimout"
 }
 </script>
+
+<!-------------------------------------------- Server Timeout Page Styling --------------------------------------------->
 
 <style scoped>
 
