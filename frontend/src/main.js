d8479d6e539f664a94420a54342628921cfd8c1d
Griffin Baxter <grb96@uclive.ac.nz>
2021-04-14 15:40:34 +1200

    S302T400-180 #time 20m Added imports for the fortawesome package, importing without the use of directories. ?fix ?chore ?pair[grb96, jlp89]
    - Fortawesome package should now import correctly

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -31,7 +31,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
@@ -39,7 +45,6 @@ import VueLogger from 'vuejs-logger';
 
 // Import bootstrap (installed using npm)
 import 'bootstrap/dist/css/bootstrap.css';
-import '../public/style.css';
 
 const options = {
   isEnabled: true,
