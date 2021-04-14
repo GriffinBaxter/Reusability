b0a2a8ddd982afdca7781fa51f53d4e74cf62f48
Griffin Baxter <grb96@uclive.ac.nz>
2021-04-14 17:03:57 +1200

    S302T400-180 #time 20m Updated the CORS URL, removed the catchall in index.js, should no longer be needed. ?fix ?chore ?pair[grb96, jlp89]

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -68,11 +68,6 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import('../views/BusinessRegistration.vue')
-    },
-    {
-        path: '*',
-        name: 'catchAll',
-        component: () => import('../views/Login')
     }
 ]
 
