7c5d4074c540d206c3ff45d5d0c5e68485cda906
Griffin Baxter <grb96@uclive.ac.nz>
2021-04-14 17:08:46 +1200

    S302T400-180 #time 5m Reverted the change for removing the catchAll, which is confirmed to be required. ?fix ?chore ?pair[grb96, jlp89]

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -68,6 +68,11 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import('../views/BusinessRegistration.vue')
+    },
+    {
+        path: '*',
+        name: 'catchAll',
+        component: () => import('../views/Login')
     }
 ]
 
