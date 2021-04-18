717651c9e38da83e18beb6db1588368801c7fa10
Dan Ronen <dro80@uclive.ac.nz>
2021-04-18 13:58:45 +1200

    S302T400-110 #time 30m Added LoadFileByFileName function to the storageService functionality ?implement

--- backend/src/main/java/org/seng302/business/product/images/StorageService.java
+++ backend/src/main/java/org/seng302/business/product/images/StorageService.java
@@ -1,13 +1,16 @@
 package org.seng302.business.product.images;
 
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 @Service
 public class StorageService implements StorageServiceInterface{
@@ -24,6 +27,24 @@ public class StorageService implements StorageServiceInterface{
         Files.copy(file.getInputStream(), this.rootPath.resolve(fileName));
     }
 
-    // Implement load image as resource method
+
+    @Override
+    public Optional<Resource> loadFileByFileName(String fileName) {
+        Optional<Resource> resource = Optional.empty();
+
+        try {
+            Path file = rootPath.resolve(fileName);
+            resource = Optional.of(new UrlResource(file.toUri()));
+
+            if (resource.get().exists() || resource.get().isReadable()) {
+                return resource;
+            } else {
+                return Optional.empty();
+            }
+        } catch (MalformedURLException error) {
+            return Optional.empty();
+        }
+
+    }
 
 }
