eb9f3f75d6aed14c4247857914765480911c4180
Zachary Kaye <zck10@uclive.ac.nz>
2021-04-20 11:42:08 +1200

    S302T400-30 #time 20m The create product button opens the create product modal when clicked. ?implement

--- frontend/src/views/Products.vue
+++ frontend/src/views/Products.vue
@@ -3,7 +3,9 @@
 
     <div class="row mb-3">
       <div class="col">
-          <button class="btn btn-outline-primary float-end mt-4" tabindex="2" id="createProductButton">Create Product</button>
+        <!--button to open create product modal-->
+        <button id="create-product-button" type="button" class="btn btn-md btn-primary float-lg-end" tabindex="2"
+                @click="modal.show()">Create Product</button>
       </div>
     </div>
 
@@ -81,55 +83,6 @@
       </div>
     </div>
 
-
-  </div>
-</template>
-
-<script>
-
-import Api from '../Api';
-import Cookies from 'js-cookie';
-
-export default {
-  name: "Products",
-  data() {
-    return {
-      productIdAscending: false,
-      nameAscending: false,
-      descriptionAscending: false,
-      manufacturerAscending: false,
-      recommendedRetailPriceAscending: false,
-      createdAscending: false,
-      rowsPerPage: 5,
-      currentPage: 1,
-      maxPage: 2,
-      productList: [],
-      small: false
-    }
-  },
-  methods: {
-
-    /**
-     * Toggles the disabling of pagination buttons.
-     *
-     * @param baseClasses Base classes to add
-     * @param condition Given condition for toggling
-     * @returns {array} A list classes to apply
-     */
-    toggleDisableClass(baseClasses, condition) {
-      const classList = [baseClasses]
-      if (condition) {
-        classList.push('disabled')
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-3 m-3">
-        <img id="logo" src="../../public/logo_only_med.png" class="img-fluid" alt="logo">
-      </div>
-    </div>
-
-    <!--button to open create product modal-->
-    <button id="create-product-button" type="button" class="btn btn-lg btn-primary float-lg-end" @click="modal.show()">Create Product</button>
-
     <!--create product modal-->
     <div class="modal fade" ref="CreateProductModal" tabindex="-1" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
@@ -182,49 +135,81 @@ export default {
         </div>
       </div>
     </div>
+
+
   </div>
 </template>
 
 <script>
 import { Modal } from 'bootstrap'
-import Api, {Product} from "../Api";
+import Api, { Product } from '../Api';
+import Cookies from 'js-cookie';
 
 export default {
   name: "Products",
-  data: () => ({
-    modal: null,
+  data() {
+    return {
+      productIdAscending: false,
+      nameAscending: false,
+      descriptionAscending: false,
+      manufacturerAscending: false,
+      recommendedRetailPriceAscending: false,
+      createdAscending: false,
+      rowsPerPage: 5,
+      currentPage: 1,
+      maxPage: 2,
+      productList: [],
+      small: false,
 
-    // Used for having pre-filled input fields
-    DEBUG_MODE: false,
+      modal: null,
 
-    // A copy of the product config file for error checking.
-    config: Product.config,
+      // Used for having pre-filled input fields
+      DEBUG_MODE: false,
 
-    // Product id related variables
-    productID: "",
-    productIDErrorMsg: "",
+      // A copy of the product config file for error checking.
+      config: Product.config,
 
-    // Product name related variables
-    productName: "",
-    productNameErrorMsg: "",
+      // Product id related variables
+      productID: "",
+      productIDErrorMsg: "",
 
-    // Recommended retail price related variables
-    recommendedRetailPrice: "",
-    recommendedRetailPriceErrorMsg: "",
+      // Product name related variables
+      productName: "",
+      productNameErrorMsg: "",
 
-    // Product description related variables
-    description: "",
-    descriptionErrorMsg: "",
+      // Recommended retail price related variables
+      recommendedRetailPrice: "",
+      recommendedRetailPriceErrorMsg: "",
 
-    // Product manufacturer related variables
-    manufacturer: "",
-    manufacturerErrorMsg: "",
+      // Product description related variables
+      description: "",
+      descriptionErrorMsg: "",
 
-    // Toast related variables
-    toastErrorMessage: "",
-    cannotProceed: false,
-  }),
+      // Product manufacturer related variables
+      manufacturer: "",
+      manufacturerErrorMsg: "",
+
+      // Toast related variables
+      toastErrorMessage: "",
+      cannotProceed: false,
+    }
+  },
   methods: {
+
+    /**
+     * Toggles the disabling of pagination buttons.
+     *
+     * @param baseClasses Base classes to add
+     * @param condition Given condition for toggling
+     * @returns {array} A list classes to apply
+     */
+    toggleDisableClass(baseClasses, condition) {
+      const classList = [baseClasses]
+      if (condition) {
+        classList.push('disabled')
+      }
+    },
+
     /**
      * This method toggles the appearance of the error message, where the is-invalid class is added to the messages
      * if an error message needs to be presented to the user.
@@ -664,7 +649,176 @@ export default {
     },
   },
 
+  /**
+   * This method checks whether the given value, val, is within the given lower and upper bounds, inclusive.
+   *
+   * @param val, int, the value to be tested for being within the range.
+   * @param min, int, the minimum value in the range.
+   * @param max, int, the maximum value in the range.
+   * @returns Boolean, true if within range, false is not within range.
+   */
+  between(val, min, max) {
+    return min <= val && val <= max;
+  },
+
+  /**
+   * This method determines the error message to be generated for a given input value based on the field type and
+   * its associated validity (determined by a regex).
+   *
+   * @param name, string, name of the input field.
+   * @param inputVal, string, the value entered in the stated field.
+   * @param minLength, number, the minimum allowed length of the inputVal.
+   * @param maxLength, number, the maximum allowed length of the inputVal.
+   * @param regexMessage, string, the tailored message about the expected syntax for the inputVal if it does not
+   *                              meet the regex given.
+   * @param regex, string, the allowed format for the given input field.
+   * @returns {string}, errorMessage, the message that needs to be raised if the inputVal does not meet the regex.
+   */
+  getErrorMessage(name, inputVal, minLength, maxLength, regexMessage = "", regex = /^[\s\S]*$/) {
+    let errorMessage = ""; //TODO: remove after testing and just have ""
+    if (inputVal === "" && minLength >= 1) {
+      errorMessage = "Please enter input";
+    }
+    else if (!regex.test(inputVal)) {
+      errorMessage = regexMessage;
+    } else if (!this.between(inputVal.length, minLength, maxLength)) {
+      errorMessage = `Input must be between ${minLength} and ${maxLength} characters long.`
+    }
+    return errorMessage;
+  },
+
+  /**
+   * This method removes white space from the beginning and end of all the input field's input values.
+   */
+  trimTextInputFields () {
+    this.productID = this.productID.trim();
+    this.productName = this.productName.trim();
+    this.recommendedRetailPrice = this.recommendedRetailPrice.trim();
+    this.manufacturer= this.manufacturer.trim();
+    this.description = this.description.trim();
+  },
+
+  /**
+   * This method creates a new product.
+   * @param e, the current event.
+   */
+  addNewProduct(e) {
+    // Steps required for the function before starting processing.
+    e.preventDefault()  // prevents page from reloading
+    this.trimTextInputFields()
+    let requestIsInvalid = false
+
+    // ===================================== START OF INPUT FIELDS VALIDATION ========================================
+
+    // Product id error checking
+    this.productIDErrorMsg = this.getErrorMessage(
+        this.config.productID.name,
+        this.productID,
+        this.config.productID.minLength,
+        this.config.productID.maxLength,
+        this.config.productID.regexMessage,
+        this.config.productID.regex
+    )
+    if (this.productIDErrorMsg) {
+      requestIsInvalid = true
+    }
+
+    // Product name error checking
+    this.productNameErrorMsg = this.getErrorMessage(
+        this.config.productName.name,
+        this.productName,
+        this.config.productName.minLength,
+        this.config.productName.maxLength,
+        this.config.productName.regexMessage,
+        this.config.productName.regex
+    )
+    if (this.productNameErrorMsg) {
+      requestIsInvalid = true
+    }
+
+    // Recommended retail price error checking
+    this.recommendedRetailPriceErrorMsg = this.getErrorMessage(
+        this.config.recommendedRetailPrice.name,
+        this.recommendedRetailPrice,
+        this.config.recommendedRetailPrice.minLength,
+        this.config.recommendedRetailPrice.maxLength
+    )
+    if (this.recommendedRetailPriceErrorMsg) {
+      requestIsInvalid = true
+    }
+
+    // Product manufacturer error checking
+    this.manufacturerErrorMsg = this.getErrorMessage(
+        this.config.manufacturer.name,
+        this.manufacturer,
+        this.config.manufacturer.minLength,
+        this.config.manufacturer.maxLength,
+        this.config.manufacturer.regexMessage,
+        this.config.manufacturer.regex
+    )
+    if (this.manufacturerErrorMsg) {
+      requestIsInvalid = true
+    }
+
+    // Description error checking
+    this.descriptionErrorMsg = this.getErrorMessage(
+        this.config.description.name,
+        this.description,
+        this.config.description.minLength,
+        this.config.description.maxLength,
+    )
+    if (this.descriptionErrorMsg) {
+      requestIsInvalid = true
+    }
+
+    // If at any stage an error has been discovered we cancel the procedure
+    if (requestIsInvalid) {
+      return
+    }
+
+    // Wrapping up the product submitted fields into a class object (Product).
+    const productData = {
+      id: this.id,
+      name: this.productName,
+      description: this.description,
+      manufacturer: this.manufacturer,
+      recommendedRetailPrice: this.recommendedRetailPrice,
+    }
+
+    const product = new Product(productData)
+
+    /*
+     * Add the Product to the database by sending an API request to the backend to store the product's information.
+     * Raise any errors and ensure they are displayed on the UI.
+     */
+    Api.addNewProduct(product
+    ).then( (res) => {
+          if (res.status === 201) {
+            this.$router.push('/businesses/:id?/products');
+          }
+        }
+    ).catch((error) => {
+      this.cannotProceed = true;
+      if (error.response) {
+        if (error.response.status === 400) {
+          this.errorMessageBubble = '400 Bad request; invalid product data';
+        } else if (error.response.status === 403) {
+          this.emailErrorMsg = 'User is not an administer of this business.'
+        } else {
+          this.errorMessageBubble = `${error.response.status} Unexpected error occurred!`;
+        }
+      } else if (error.request) {
+        this.errorMessageBubble = 'Timeout occurred';
+      } else {
+        this.errorMessageBubble = 'Unexpected error occurred!';
+      }
+    })
+  },
+
   mounted() {
+
+    // When mounted create instance of modal
+    this.modal = new Modal(this.$refs.CreateProductModal)
 
     /**
      * When mounted, initiate population of page.
@@ -682,186 +836,10 @@ export default {
     } else {
       this.$router.push({name: 'Login'});
     }
-  }
-}
-</script>
-
-<style scoped>
-
-
-
-</style>
-
-     * This method checks whether the given value, val, is within the given lower and upper bounds, inclusive.
-     *
-     * @param val, int, the value to be tested for being within the range.
-     * @param min, int, the minimum value in the range.
-     * @param max, int, the maximum value in the range.
-     * @returns Boolean, true if within range, false is not within range.
-     */
-    between(val, min, max) {
-      return min <= val && val <= max;
-    },
-
-    /**
-     * This method determines the error message to be generated for a given input value based on the field type and
-     * its associated validity (determined by a regex).
-     *
-     * @param name, string, name of the input field.
-     * @param inputVal, string, the value entered in the stated field.
-     * @param minLength, number, the minimum allowed length of the inputVal.
-     * @param maxLength, number, the maximum allowed length of the inputVal.
-     * @param regexMessage, string, the tailored message about the expected syntax for the inputVal if it does not
-     *                              meet the regex given.
-     * @param regex, string, the allowed format for the given input field.
-     * @returns {string}, errorMessage, the message that needs to be raised if the inputVal does not meet the regex.
-     */
-    getErrorMessage(name, inputVal, minLength, maxLength, regexMessage = "", regex = /^[\s\S]*$/) {
-      let errorMessage = ""; //TODO: remove after testing and just have ""
-      if (inputVal === "" && minLength >= 1) {
-        errorMessage = "Please enter input";
-      }
-      else if (!regex.test(inputVal)) {
-        errorMessage = regexMessage;
-      } else if (!this.between(inputVal.length, minLength, maxLength)) {
-        errorMessage = `Input must be between ${minLength} and ${maxLength} characters long.`
-      }
-      return errorMessage;
-    },
-
-    /**
-     * This method removes white space from the beginning and end of all the input field's input values.
-     */
-    trimTextInputFields () {
-      this.productID = this.productID.trim();
-      this.productName = this.productName.trim();
-      this.recommendedRetailPrice = this.recommendedRetailPrice.trim();
-      this.manufacturer= this.manufacturer.trim();
-      this.description = this.description.trim();
-    },
-
-    /**
-     * This method creates a new product.
-     * @param e, the current event.
-     */
-    addNewProduct(e) {
-      // Steps required for the function before starting processing.
-      e.preventDefault()  // prevents page from reloading
-      this.trimTextInputFields()
-      let requestIsInvalid = false
-
-      // ===================================== START OF INPUT FIELDS VALIDATION ========================================
-
-      // Product id error checking
-      this.productIDErrorMsg = this.getErrorMessage(
-          this.config.productID.name,
-          this.productID,
-          this.config.productID.minLength,
-          this.config.productID.maxLength,
-          this.config.productID.regexMessage,
-          this.config.productID.regex
-      )
-      if (this.productIDErrorMsg) {
-        requestIsInvalid = true
-      }
-
-      // Product name error checking
-      this.productNameErrorMsg = this.getErrorMessage(
-          this.config.productName.name,
-          this.productName,
-          this.config.productName.minLength,
-          this.config.productName.maxLength,
-          this.config.productName.regexMessage,
-          this.config.productName.regex
-      )
-      if (this.productNameErrorMsg) {
-        requestIsInvalid = true
-      }
-
-      // Recommended retail price error checking
-      this.recommendedRetailPriceErrorMsg = this.getErrorMessage(
-          this.config.recommendedRetailPrice.name,
-          this.recommendedRetailPrice,
-          this.config.recommendedRetailPrice.minLength,
-          this.config.recommendedRetailPrice.maxLength
-      )
-      if (this.recommendedRetailPriceErrorMsg) {
-        requestIsInvalid = true
-      }
-
-      // Product manufacturer error checking
-      this.manufacturerErrorMsg = this.getErrorMessage(
-          this.config.manufacturer.name,
-          this.manufacturer,
-          this.config.manufacturer.minLength,
-          this.config.manufacturer.maxLength,
-          this.config.manufacturer.regexMessage,
-          this.config.manufacturer.regex
-      )
-      if (this.manufacturerErrorMsg) {
-        requestIsInvalid = true
-      }
-
-      // Description error checking
-      this.descriptionErrorMsg = this.getErrorMessage(
-          this.config.description.name,
-          this.description,
-          this.config.description.minLength,
-          this.config.description.maxLength,
-      )
-      if (this.descriptionErrorMsg) {
-        requestIsInvalid = true
-      }
-
-      // If at any stage an error has been discovered we cancel the procedure
-      if (requestIsInvalid) {
-        return
-      }
-
-      // Wrapping up the product submitted fields into a class object (Product).
-      const productData = {
-        id: this.id,
-        name: this.productName,
-        description: this.description,
-        manufacturer: this.manufacturer,
-        recommendedRetailPrice: this.recommendedRetailPrice,
-      }
-
-      const product = new Product(productData)
-
-      /*
-       * Add the Product to the database by sending an API request to the backend to store the product's information.
-       * Raise any errors and ensure they are displayed on the UI.
-       */
-      Api.addNewProduct(product
-      ).then( (res) => {
-            if (res.status === 201) {
-              this.$router.push('/businesses/:id?/products');
-            }
-          }
-      ).catch((error) => {
-        this.cannotProceed = true;
-        if (error.response) {
-          if (error.response.status === 400) {
-            this.errorMessageBubble = '400 Bad request; invalid product data';
-          } else if (error.response.status === 403) {
-            this.emailErrorMsg = 'User is not an administer of this business.'
-          } else {
-            this.errorMessageBubble = `${error.response.status} Unexpected error occurred!`;
-          }
-        } else if (error.request) {
-          this.errorMessageBubble = 'Timeout occurred';
-        } else {
-          this.errorMessageBubble = 'Unexpected error occurred!';
-        }
-      })
-    },
-
   },
-  mounted() {
-    this.modal = new Modal(this.$refs.CreateProductModal)
-  }
-};
+
+}
+
 </script>
 
 <style scoped>
