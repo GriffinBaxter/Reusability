b6ad7f7ffce70912cfbc984eb30f70c12c8b60cd
Hayley Krippner <hek21@uclive.ac.nz>
2021-04-22 21:13:55 +1200

    S302T400-178 #time 37m SQL script for inserting a user and an address has correct syntax now but we got a "could not initialize proxy [org.seng302.address.Address#1] - no Session" error. ?implement ?pair[hek21, jlp89]

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,8 +1,6 @@
 
--- INSERT INTO address (street_number, street_name, city, region, country, postcode) VALUES ("1", "Blue Street", "Upper Hutt", "Wellington", "New Zealand", "5018")
+INSERT INTO `address` (id, street_number, street_name, city, region, country, postcode) VALUES (1, '109', 'Blue Street', 'Upper Hutt', 'Wellington', 'New Zealand', '5018');
 
-INSERT INTO user (first_name, last_name, middle_name, nickname, bio, email, created, address_id, password, date_of_birth, role)
-VALUES ("Bob", "Doe", "S", "Johnny", "Biography", "email@email.com", LocalDate.of(2000, 2, 2), "0271316", "1", "Password123!",
-        LocalDateTime.of(LocalDate.of(2021, 2, 2), LocalTime.of(0, 0)), "DEFAULTGLOBALAPPLICATIONADMIN"))
+INSERT INTO `user` (id, first_name, last_name, middle_name, nickname, bio, email, date_of_birth, phone_number, address_id, password, created, role) VALUES (2, 'Bob', 'Doe', 'S', 'Johnny', 'Biography', 'email@email.com', DATE'2000-05-14', '0271316', 1, 'Password123!', DATE'2019-05-14', 'DEFAULTGLOBALAPPLICATIONADMIN');
 
 
