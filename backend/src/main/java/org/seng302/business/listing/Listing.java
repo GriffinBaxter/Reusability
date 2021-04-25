de7f895d7d3b681fe288c41e289b5e3921934d5c
Zachary Kaye <zck10@uclive.ac.nz>
2021-04-25 16:12:05 +1200

    S302T400-54 #time 20m Created constructor and getter and setters for the Listing JPA entity ?implement ?pair[zck10, zca19]

--- backend/src/main/java/org/seng302/business/listing/Listing.java
+++ backend/src/main/java/org/seng302/business/listing/Listing.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * Class for sale listings
@@ -19,34 +20,87 @@ import java.time.LocalDate;
 public class Listing {
     @Id // this field (attribute) is the table primary key
     @GeneratedValue//(strategy = GenerationType.IDENTITY) // autoincrement the ID
-    @Column(name = "listingId", nullable = false)
-    private int listingId;
+    @Column(name = "id", nullable = false)
+    private int id;
 
-    // Foreign key: inventoryId, int
-
-    // Many-to-one relationship
-
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    @Column(name = "thumbnail_location")
-    private String thumbnail_location;
-
-    @Column (name = "start_date")
-    private LocalDate start_date;
-
-    @Column (name = "closing_date")
-    private LocalDate closing_date;
-
-    @Column (name = "expiry_date", nullable = false)
-    private LocalDate expiry_date;
-
-    @Column (name = "price_per_item")
-    private double price_per_item;
-
-    @Column (name = "total_price")
-    private double total_price;
+    // InventoryItem
 
     @Column (name = "quantity", nullable = false)
     private int quantity;
+
+    @Column (name = "price", nullable = false)
+    private double price;
+
+    @Column (name = "moreInfo")
+    private String moreInfo;
+
+    @Column (name = "created")
+    private LocalDateTime created;
+
+    @Column (name = "closes")
+    private LocalDateTime closes;
+
+    /**
+     * Create a new listing.
+     * @param quantity the quantity of an item being listed.
+     * @param price the price of the listing
+     * @param moreInfo the business may wish to include extra info about the listing e.g. willing to consider near offers.
+     * @param created the date the listing was created.
+     * @param closes the date the listing closes, defaults to expiry date.
+     */
+    public Listing(int quantity, double price, String moreInfo, LocalDateTime created, LocalDateTime closes) {
+        this.quantity = quantity;
+        this.price = price;
+        this.moreInfo = moreInfo;
+        this.created = created;
+        this.closes = closes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getMoreInfo() {
+        return moreInfo;
+    }
+
+    public void setMoreInfo(String moreInfo) {
+        this.moreInfo = moreInfo;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getCloses() {
+        return closes;
+    }
+
+    public void setCloses(LocalDateTime closes) {
+        this.closes = closes;
+    }
 }
