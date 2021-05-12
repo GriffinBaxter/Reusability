e499045d82f12a9a2d45f54fd5e9831ffc7819a4
Jack Patterson <jlp89@uclive.ac.nz>
2021-05-12 16:10:12 +1200

    Started writing login tests

--- frontend/tests/login.test.js
+++ frontend/tests/login.test.js
@@ -1,7 +1,48 @@
-import {shallowMount, createLocalVue} from "@vue/test-utils";
+import {createLocalVue, shallowMount} from "@vue/test-utils";
 import Login from "../src/views/Login"
 import Api from "../src/Api"
+import VueLogger from "vuejs-logger"
+import VueRouter from 'vue-router'
+import router from '../src/router/index'
 
 jest.mock("../src/Api");
+
+const localVue = createLocalVue();
+localVue.use(VueLogger, {isEnabled : false});
+localVue.use(VueRouter);
+
+
+let loginWrapper;
+
+describe("Clicking on Sign in button", () => {
+
+    let signInButton;
+
+    beforeEach(async () => {
+
+        let signInSuccess = {
+            "data": {
+                userID: 1
+            }
+        }
+        Api.signIn.mockImplementation(() => Promise.resolve(signInSuccess))
+
+        loginWrapper = await shallowMount(Login, {localVue, router});
+
+        await loginWrapper.vm.$nextTick();
+
+        signInButton = loginWrapper.get("#loginButton")
+    })
+
+    test("Successful login with valid credentials", async () => {
+
+        await signInButton.trigger("click");
+
+        expect(loginWrapper.vm.$route.name).toStrictEqual(0);
+        // Expect route change
+
+    })
+
+})
 
 
