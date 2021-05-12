445bdb28136a1633ba7d39eef05bf2c6f79eb9c4
Zachary Kaye <zck10@uclive.ac.nz>
2021-05-12 13:08:53 +1200

    S302T400-355 #time 20m Added more addresses to the SQL script. ?implement

--- backend/src/main/resources/data-h2.sql
+++ backend/src/main/resources/data-h2.sql
@@ -1,4 +1,4 @@
-/* Addresses for Users and Businesses. */
+/* Addresses for Users (20) */
 INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('109', 'Blue Street', 'Upper Hutt', 'Wellington', 'New Zealand', '5018');
 INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('333', 'Ilam Road', 'Christchurch', 'Canterbury', 'New Zealand', '90210');
 INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('57', 'Sydney Highway', 'Shire of Cocos Islands', 'West Island', 'Cocos (Keeling) Islands', '9732');
@@ -7,9 +7,18 @@ INSERT INTO `address` (street_number, street_name, city, region, country, postco
 INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('80416', 'Rodney Street', 'Jon Loop', 'Shaanxi', 'China', '2113');
 INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('9205', 'Monique Vista', 'Bururi', 'Bigomogomo', 'Africa', '1000');
 INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('240', 'Newson Street', 'Bernhard Run', 'Southland', 'New Zealand', '2839');
-INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('157', 'Lung Cheung Road', 'Kowloon', 'Hong Kong', 'China', '1602');
-INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('16', 'Rue du Cercle', 'Entzheim', 'Grand Est', 'France', '67960');
-INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('133', 'Waima Road', 'Shantou', 'Guangdong Province', 'China', '515031');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('16', 'Presidente Peron', 'Funes', 'Santa Fe', 'Argentina', 'S2132');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('190', 'Fort Washington Avenue', 'New York', 'New York', 'United States', '10040');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('32', 'Hunter Avenue', 'Fairview Shores', 'Florida', 'United States', '32804');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('32', 'Via Giuseppe Di Vittorio', 'Rovereto', 'Trentino-Alto Adige/Südtirol', 'Italy', '38068');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('3434', 'Russell Street', 'Detroit', 'Michigan', 'United States', '48207');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('16', 'Barnards Road', 'Scarborough', 'England', 'United Kingdom', 'YO21 1UX');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('199', 'Kwa Jongo Street', 'Dar es Salaam', 'Coastal Zone', 'Tanzania', '78570');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('76', 'Milltown Road', 'Strabane', 'Northern Ireland', 'United Kingdom', 'BT82 8AS');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('87', 'Ansancheonnam-ro', 'Ansan-si', 'Gyeonggi-do', 'South Korea', '15483');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('98', 'Sqaq il-Qenċ', 'Zebbug', 'Southern Region', 'Malta', 'ZBG9019');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('19', 'Heping Road', 'Zebbug', 'Tianjin', 'China', '300010');
+INSERT INTO `address` (street_number, street_name, city, region, country, postcode) VALUES ('19b', 'IGIS Road', 'Awan Town', 'Islamabad Capital Territory', 'Pakistan', '44110');
 
 /* Users */
 INSERT INTO `user` (first_name, last_name, middle_name, nickname, bio, email, date_of_birth, phone_number, address_id, password, created, role) VALUES ('Alex', 'Doe', 'Joe', 'Johnny', 'Biography', 'emailUSER@email.com', DATE'2008-02-02', '0271317', 5, 'UGFzc3dvcmQxMjMh', DATE'2021-02-12', 'USER');
@@ -21,12 +30,12 @@ INSERT INTO `user` (first_name, last_name, middle_name, nickname, bio, email, da
 INSERT INTO `user` (first_name, last_name, middle_name, nickname, bio, email, date_of_birth, phone_number, address_id, password, created, role) VALUES ('Francisca', 'Bznitez', 'Tessa', 'Fran', 'Biography', 'francisca.bznitez@example.com', DATE'2001-07-17', '967352531', 8, 'UGFzc3dvcmQxMjMh', DATE'2021-01-01', 'USER');
 INSERT INTO `user` (first_name, last_name, middle_name, nickname, bio, email, date_of_birth, phone_number, address_id, password, created, role) VALUES ('Frank', 'Smith', 'J', 'Frankie', 'Biography', 'frank.j.smith@email.com', DATE'2000-05-14', '0271316', 1, 'UGFzc3dvcmQxMjMh', DATE'2019-05-14', 'USER');
 
-/* Businesses */
-INSERT INTO `business` (primary_administrator_id, name, description, address_id, business_type, created) VALUES (1, 'Brink Food', 'Description', 9, 'RETAIL_TRADE', DATE'2021-02-12');
-INSERT INTO `business` (primary_administrator_id, name, description, address_id, business_type, created) VALUES (2, 'Sunburst Waste', 'Description', 10, 'CHARITABLE_ORGANISATION', DATE'2021-05-14');
-INSERT INTO `business` (primary_administrator_id, name, description, address_id, business_type, created) VALUES (3, 'Fringe Wasteless', 'Description', 11, 'RETAIL_TRADE', DATE'2021-05-01');
-
-/* Link Users to Businesses */
-INSERT INTO `users_businesses` (user_id, businesses_id) VALUES (1, 1);
-INSERT INTO `users_businesses` (user_id, businesses_id) VALUES (2, 2);
-INSERT INTO `users_businesses` (user_id, businesses_id) VALUES (3, 3);
+-- /* Businesses */
+-- INSERT INTO `business` (primary_administrator_id, name, description, address_id, business_type, created) VALUES (1, 'Brink Food', 'Description', 9, 'RETAIL_TRADE', DATE'2021-02-12');
+-- INSERT INTO `business` (primary_administrator_id, name, description, address_id, business_type, created) VALUES (2, 'Sunburst Waste', 'Description', 10, 'CHARITABLE_ORGANISATION', DATE'2021-05-14');
+-- INSERT INTO `business` (primary_administrator_id, name, description, address_id, business_type, created) VALUES (3, 'Fringe Wasteless', 'Description', 11, 'RETAIL_TRADE', DATE'2021-05-01');
+--
+-- /* Link Users to Businesses */
+-- INSERT INTO `users_businesses` (user_id, businesses_id) VALUES (1, 1);
+-- INSERT INTO `users_businesses` (user_id, businesses_id) VALUES (2, 2);
+-- INSERT INTO `users_businesses` (user_id, businesses_id) VALUES (3, 3);
