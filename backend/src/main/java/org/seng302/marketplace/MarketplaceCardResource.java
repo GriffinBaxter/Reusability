fa41b67fac73c6905b0fb8821690aed272237b9c
Zachary Kaye <zck10@uclive.ac.nz>
2021-05-14 17:19:17 +1200

    Wrote TODO's for MarketplaceCardResource.

--- backend/src/main/java/org/seng302/marketplace/MarketplaceCardResource.java
+++ backend/src/main/java/org/seng302/marketplace/MarketplaceCardResource.java
@@ -1,8 +1,15 @@
 package org.seng302.marketplace;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.seng302.business.BusinessResource;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MarketplaceCardResource {
 
+    // TODO autowire the needed repositories, when implementing endpoints.
+    // TODO create constructor for MarketplaceCardResource with Autowired repositories.
+
+    private static final Logger logger = LogManager.getLogger(BusinessResource.class.getName());
 }
