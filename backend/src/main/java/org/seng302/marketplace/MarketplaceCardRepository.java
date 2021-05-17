97c2e86a6315a544a677d22c6e3f9d22a6b7dfff
Zachary Kaye <zck10@uclive.ac.nz>
2021-05-17 12:57:28 +1200

    S302T400-328 #time 30m Began writing integration tests for the POST /cards endpoint (yet to be implemented) ?test

--- backend/src/main/java/org/seng302/marketplace/MarketplaceCardRepository.java
+++ backend/src/main/java/org/seng302/marketplace/MarketplaceCardRepository.java
@@ -1,9 +1,27 @@
 package org.seng302.marketplace;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
 
 @RepositoryRestResource
 public interface MarketplaceCardRepository extends JpaRepository<MarketplaceCard, Integer> {
 
+    /**
+     * Search to see if a card exists.
+     * Useful for validation purposes when creating a card.
+     * @param creatorId the id of the user who created the card.
+     * @param section the section of the marketplace the card is to be placed in (For Sale etc.)
+     * @param title the title of the card.
+     * @param description the description of the card.
+     * @return address object if exists
+     */
+    Optional<MarketplaceCard> findMarketplaceCardByCreatorIdAndSectionAndTitleAndDescription(
+            Integer creatorId,
+            Section section,
+            String title,
+            String description
+    );
 }
---
+++ backend/src/test/java/org/seng302/marketplace/MarketplaceCardResourceIntegrationTests.java
@@ -0,0 +1,177 @@
+package org.seng302.marketplace;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.seng302.address.Address;
+import org.seng302.address.AddressRepository;
+import org.seng302.main.Main;
+import org.seng302.user.Role;
+import org.seng302.user.User;
+import org.seng302.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.servlet.http.Cookie;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+/**
+ * MarketplaceCardResource test class
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {Main.class})
+@ActiveProfiles("test")
+public class MarketplaceCardResourceIntegrationTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private MarketplaceCardRepository cardRepository;
+
+    @MockBean
+    private AddressRepository addressRepository;
+
+
+    private MockHttpServletResponse response;
+
+    private final String cardPayloadJson = "{\"creatorId\":\"%d\"," +
+            "\"section\":\"%s\"," +
+            "\"title\":\"%s\"," +
+            "\"description\":\"%s\"}"; // TODO update when we have keywords.
+
+    private String payloadJson;
+
+    private User user;
+    private MarketplaceCard card;
+
+    /**
+     * Before all create a user that will be used in all tests when creating cards.
+     * @throws Exception thrown if there is an error when creating an address or user.
+     */
+    @BeforeAll
+    public void setup() throws Exception {
+        Address address = new Address(
+                "3/24",
+                "Ilam Road",
+                "Christchurch",
+                "Canterbury",
+                "New Zealand",
+                "90210"
+        );
+
+        user = new User("first",
+                "last",
+                "middle",
+                "nick",
+                "bio",
+                "example@example.com",
+                LocalDate.of(2000, 1, 1),
+                "123456789",
+                address,
+                "Password123!",
+                LocalDateTime.of(LocalDate.of(2021, 1, 1),
+                        LocalTime.of(0, 0)),
+                Role.USER);
+        user.setId(1);
+        user.setSessionUUID(User.generateSessionUUID());
+
+        this.mvc = MockMvcBuilders.standaloneSetup(new MarketplaceCardResource(
+                cardRepository, userRepository, addressRepository))
+                .build();
+    }
+
+    /**
+     * Tests that a CREATED status is received when sending a marketplace card creation payload to the
+     * /cards API endpoint that contains a card with valid data.
+     * @throws Exception thrown if there is an error when creating a card.
+     */
+    @Test
+    public void canCreateCardWhenUserExistsAndDataValid() throws Exception {
+        // given
+        given(userRepository.findById(1)).willReturn(Optional.ofNullable(user));
+
+        card = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.FORSALE,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 1), LocalTime.of(0, 0)),
+                "Hayley's Birthday",
+                "Come join Hayley and help her celebrate her birthday!"
+        );
+
+        payloadJson = String.format(cardPayloadJson, card.getCreatorId(), card.getSection(), card.getTitle(), card.getDescription());
+        given(cardRepository.findMarketplaceCardByCreatorIdAndSectionAndTitleAndDescription(
+                card.getCreatorId(), card.getSection(), card.getTitle(), card.getDescription()))
+                .willReturn(Optional.empty());
+
+        // when
+        when(userRepository.findBySessionUUID(user.getSessionUUID())).thenReturn(Optional.ofNullable(user));
+        when(cardRepository.save(any(MarketplaceCard.class))).thenReturn(card);
+        response = mvc.perform(post("/cards")
+                .contentType(MediaType.APPLICATION_JSON).content(payloadJson)
+                .cookie(new Cookie("JSESSIONID", user.getSessionUUID())))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    /**
+     * Tests that a BAD_REQUEST status is received when sending a marketplace card creation payload to the
+     * /cards API endpoint that contains a card that already exists for an
+     * existing creator ID.
+     * @throws Exception thrown if there is an error when creating a card.
+     */
+    @Test
+    public void cantCreateCardWhenCreatorExistsButCardAlreadyExists() throws Exception {
+        // given
+        card = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.FORSALE,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 1), LocalTime.of(0, 0)),
+                "Hayley's Birthday",
+                "Come join Hayley and help her celebrate her birthday!"
+        );
+
+        given(userRepository.findById(1)).willReturn(Optional.ofNullable(user));
+        given(cardRepository.findMarketplaceCardByCreatorIdAndSectionAndTitleAndDescription(
+                card.getCreatorId(), card.getSection(), card.getTitle(), card.getDescription()))
+                .willReturn(Optional.ofNullable(card));
+        payloadJson = String.format(cardPayloadJson, card.getCreatorId(), card.getSection(), card.getTitle(), card.getDescription());
+
+        // when
+        when(userRepository.findBySessionUUID(user.getSessionUUID())).thenReturn(Optional.ofNullable(user));
+        response = mvc.perform(post(String.format("/cards"))
+                .contentType(MediaType.APPLICATION_JSON).content(payloadJson)
+                .cookie(new Cookie("JSESSIONID", user.getSessionUUID())))
+                .andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
