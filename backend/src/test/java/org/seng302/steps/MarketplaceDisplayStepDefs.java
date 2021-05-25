77e647b0ec8f7c7e439c5ecb249327616e182a02
Jack Patterson <jlp89@uclive.ac.nz>
2021-05-25 16:32:26 +1200

    S302T400-405 #time 30m Began to implement cucumber tests for UCM3. Specifically wrote three scenarios for AC1 and the given step definition for each of them. ?test ?pair[jlp89, hek21]

--- backend/src/test/java/org/seng302/steps/MarketplaceDisplayStepDefs.java
+++ backend/src/test/java/org/seng302/steps/MarketplaceDisplayStepDefs.java
@@ -11,6 +11,7 @@ import org.seng302.user.User;
 import org.seng302.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +22,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -48,6 +50,17 @@ public class MarketplaceDisplayStepDefs extends CucumberSpringConfiguration {
 
 
     private MarketplaceCard marketplaceCard;
+    private MarketplaceCard marketplaceCard1;
+    private MarketplaceCard marketplaceCard2;
+    private MarketplaceCard marketplaceCard3;
+    private MarketplaceCard marketplaceCard4;
+    private MarketplaceCard marketplaceCard5;
+    private MarketplaceCard marketplaceCard6;
+    private MarketplaceCard marketplaceCard7;
+    private MarketplaceCard marketplaceCard8;
+    private MarketplaceCard marketplaceCard9;
+
+
     private User user;
     private Address address;
 
@@ -146,5 +159,166 @@ public class MarketplaceDisplayStepDefs extends CucumberSpringConfiguration {
                 user.getCreated(), user.getRole(), user.getHomeAddress().toSecureString(), marketplaceCard.getSection().toString(),
                 marketplaceCard.getCreated(), marketplaceCard.getDisplayPeriodEnd(), marketplaceCard.getTitle(),
                 marketplaceCard.getDescription(), marketplaceCard.getKeywords()));
+    }
+
+    @Given("There are three cards in each of the Wanted, For Sale, and Exchange sections.")
+    public void there_are_three_cards_in_each_of_the_wanted_for_sale_and_exchange_sections() throws Exception {
+
+        address = new Address(
+                "3/24",
+                "Ilam Road",
+                "Christchurch",
+                "Canterbury",
+                "New Zealand",
+                "90210",
+                "Ilam"
+        );
+
+        user = new User(
+                "John",
+                "Doe",
+                "S",
+                "Generic",
+                "Biography",
+                "email@email.com",
+                LocalDate.of(2000, 2, 2),
+                "0271316",
+                address,
+                "Password123!",
+                LocalDateTime.of(LocalDate.of(2021, 2, 2),
+                        LocalTime.of(0, 0)),
+                Role.USER);
+        user.setId(1);
+        user.setSessionUUID(User.generateSessionUUID());
+
+        // ************* For Sale Events *************
+
+        marketplaceCard1 = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.FORSALE,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 1), LocalTime.of(0, 0)),
+                "Hayley's Birthday",
+                "Come join Hayley and help her celebrate her birthday!"
+        );
+        marketplaceCard1.setId(1);
+
+        marketplaceCard2 = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.FORSALE,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 2), LocalTime.of(0, 0)),
+                "Card 2",
+                "Card 2 description"
+        );
+        marketplaceCard2.setId(2);
+
+        marketplaceCard3 = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.FORSALE,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 3), LocalTime.of(0, 0)),
+                "Card 3",
+                "Card 3 description"
+        );
+        marketplaceCard3.setId(3);
+
+        // ************* Wanted Events *************
+
+        marketplaceCard4 = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.WANTED,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 4), LocalTime.of(0, 0)),
+                "Card 4",
+                "Card 4 description"
+        );
+        marketplaceCard4.setId(4);
+
+        marketplaceCard5 = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.WANTED,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 5), LocalTime.of(0, 0)),
+                "Card 5",
+                "Card 5 description"
+        );
+        marketplaceCard5.setId(5);
+
+        marketplaceCard6 = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.WANTED,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 6), LocalTime.of(0, 0)),
+                "Card 6",
+                "Card 6 description"
+        );
+        marketplaceCard6.setId(6);
+
+        // ************* Exchange Events *************
+
+        marketplaceCard7 = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.EXCHANGE,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 7), LocalTime.of(0, 0)),
+                "Card 7",
+                "Card 7 description"
+        );
+        marketplaceCard7.setId(7);
+
+        marketplaceCard8 = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.EXCHANGE,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 7), LocalTime.of(0, 0)),
+                "Card 8",
+                "Card 8 description"
+        );
+        marketplaceCard8.setId(8);
+
+        marketplaceCard9 = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.EXCHANGE,
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 9), LocalTime.of(0, 0)),
+                "Card 9",
+                "Card 9 description"
+        );
+        marketplaceCard9.setId(9);
+
+        given(userRepository.findBySessionUUID(user.getSessionUUID())).willReturn(Optional.ofNullable(user));
+
+        List<MarketplaceCard> list = List.of(marketplaceCard1, marketplaceCard2, marketplaceCard3,
+                marketplaceCard4, marketplaceCard5, marketplaceCard6, marketplaceCard7,
+                marketplaceCard8, marketplaceCard9);
+        Page<MarketplaceCard> pagedResponse = new PageImpl<>(list);
+        Pageable paging = PageRequest.of(0, 10, Sort.by(Sort.Order.desc("created").ignoreCase()));
+        given(marketplaceCardRepository.findAllBySection(Section.EXCHANGE, paging)).willReturn(pagedResponse);
+        given(marketplaceCardRepository.findAllBySection(Section.WANTED, paging)).willReturn(pagedResponse);
+        given(marketplaceCardRepository.findAllBySection(Section.FORSALE, paging)).willReturn(pagedResponse);
+
+        assertThat(marketplaceCardRepository.findAllBySection(Section.EXCHANGE, paging)).isNotEmpty();
+        assertThat(marketplaceCardRepository.findAllBySection(Section.WANTED, paging)).isNotEmpty();
+        assertThat(marketplaceCardRepository.findAllBySection(Section.FORSALE, paging)).isNotEmpty();
+
+    }
+
+    @When("The user attempts to view the {string} section.")
+    public void the_user_attempts_to_view_the_section(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Only the {string} section cards are retrieved.")
+    public void only_the_section_cards_are_retrieved(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The most recently created \\(or renewed) items appear first.")
+    public void the_most_recently_created_or_renewed_items_appear_first() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 }
--- backend/src/test/resources/features/marketplaceDisplay.feature
+++ backend/src/test/resources/features/marketplaceDisplay.feature
@@ -1,7 +1,24 @@
 Feature: UCM3 - Marketplace Section Display
 
+  Scenario: AC1 - I only see cards from the Wanted section when wanted is selected
+    Given There are three cards in each of the Wanted, For Sale, and Exchange sections.
+    When The user attempts to view the "Wanted" section.
+    Then Only the "Wanted" section cards are retrieved.
+    And The most recently created (or renewed) items appear first.
+
+  Scenario: AC1 - I only see cards from the Exchange section when wanted is selected
+    Given There are three cards in each of the Wanted, For Sale, and Exchange sections.
+    When The user attempts to view the "Exchange" section.
+    Then Only the "Exchange" section cards are retrieved.
+    And The most recently created (or renewed) items appear first.
+
+  Scenario: AC1 - I only see cards from the For Sale section when wanted is selected
+    Given There are three cards in each of the Wanted, For Sale, and Exchange sections.
+    When The user attempts to view the "For Sale" section.
+    Then Only the "For Sale" section cards are retrieved.
+    And The most recently created (or renewed) items appear first.
+
   Scenario: AC4 - You can view the full card content of a given card.
     Given A card with ID 1 exists in the database.
     When The user attempts to retrieve the details for the card with ID 1.
     Then The card with ID 1 is retrieved.
-    
