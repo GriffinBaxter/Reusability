895e7e4d5fa58fdd43f1be4198560b075f750d05
Troy Tomlins <tpt38@uclive.ac.nz>
2021-07-13 19:45:56 +1200

    Revert "S302T400-506 #time 2h Created Delete endpoint for Keywords ?implement"
    This reverts commit 3320fe5d710f27edbd77b18d23d4fbb09be34ab6

--- backend/src/main/java/org/seng302/controller/KeywordResource.java
+++ backend/src/main/java/org/seng302/controller/KeywordResource.java
@@ -15,8 +15,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.seng302.Authorization;
 import org.seng302.model.Keyword;
-import org.seng302.model.MarketplaceCard;
-import org.seng302.model.User;
 import org.seng302.model.repository.KeywordRepository;
 import org.seng302.model.repository.UserRepository;
 import org.seng302.view.incoming.KeywordCreationPayload;
@@ -24,18 +22,19 @@ import org.seng302.view.outgoing.KeywordIdPayload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 /**
  * Keyword Resource class.
  * This class contains the endpoints for keywords.
  * The POST /keywords endpoint is used to create keywords.
- * The DELETE /keywords/{id} endpoint is used to delete keywords.
  */
 @RestController
 public class KeywordResource {
@@ -88,48 +87,5 @@ public class KeywordResource {
                     e.getMessage()
             );
         }
-    }
-
-    /**
-     * DELETE endpoint for deleting keywords (DGAA/GAA's only)
-     * @param id Keyword ID
-     * @param sessionToken JSESSIONID for verifying the user is logged in
-     */
-    @DeleteMapping("/keywords/{id}")
-    @ResponseStatus(code = HttpStatus.OK, reason = "Keyword Successfully deleted")
-    public void deleteKeyword( @PathVariable Integer id,
-            @CookieValue(value = "JSESSIONID", required = false) String sessionToken
-    ) {
-        logger.info("Request to delete keyword: {}", id);
-
-        // Checks if user is logged in
-        User currentUser = Authorization.getUserVerifySession(sessionToken, userRepository);
-
-        // Checks user is GAA/DGAA
-        if (!Authorization.isGAAorDGAA(currentUser)) {
-            logger.error("Keyword Deletion Error - 403 [FORBIDDEN] - User doesn't have permissions to delete keywords");
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid permissions to delete keywords");
-        }
-
-        logger.debug("Keyword Deletion Update - User has permissions to delete keywords");
-
-        Optional<Keyword> keyword = keywordRepository.findById(id);
-
-        if (keyword.isEmpty()) {
-            logger.error("Keyword Deletion Error - 400 [BAD_REQUEST] - Keyword at ID {} not found", id);
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Keyword not found");
-        }
-
-        logger.debug("Keyword Deletion Update - Keyword found");
-
-        List<MarketplaceCard> cards = keyword.get().getCards();
-        for (MarketplaceCard card: cards) {
-            card.removeKeyword(keyword.get());
-        }
-
-        logger.debug("Keyword Deletion Update - Keyword removed from cards");
-
-        keywordRepository.delete(keyword.get());
-        logger.info("Keyword Deletion - 200 [OK] - Keyword at id {} successfully deleted", id);
     }
 }
