a5e5fe47967116666826a005da307d4d87d5efca
Zachary Kaye <zck10@uclive.ac.nz>
2021-07-18 15:47:21 +1200

    S302T400-507 #time 10m Created a POSTMAN collection with user search examples. ?document

--- backend/src/main/java/org/seng302/model/repository/UserRepository.java
+++ backend/src/main/java/org/seng302/model/repository/UserRepository.java
@@ -12,11 +12,8 @@ package org.seng302.model.repository;
 
 import org.seng302.model.enums.Role;
 import org.seng302.model.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
