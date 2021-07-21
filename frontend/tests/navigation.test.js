46658c9c651be9bc9547b90141ff8d45a90db8cc
Hayley Krippner <hek21@uclive.ac.nz>
2021-07-21 15:27:55 +1200

    S302T400-511 #time 1h Billie and I worked on writing tests for the side nav bar and top nav bar and Billie explained to me with how to mock routers, routers and do stubbing. We ran into issues with the tests not finishing when we used stubbing and need to do research to resolve this. ?pair[hek21, baj40] ?test

--- frontend/tests/navigation.test.js
+++ frontend/tests/navigation.test.js
@@ -4,10 +4,61 @@
  */
 
 import {test, expect, describe} from "@jest/globals"
-import {shallowMount} from "@vue/test-utils";
+import {mount} from "@vue/test-utils";
 import Navbar from "../src/components/main/Navbar";
 import SideNavBar from "../src/components/main/SideNavBar";
 import Home from "../src/views/Home"
+
+import { RouterLinkStub } from '@vue/test-utils';
+
+let navbarWrapper;
+let sideNavBarWrapper;
+let homeWrapper;
+let sideNavToggleButton;
+let sideNavBar;
+let homePage;
+let sideNavCloseButton;
+
+beforeAll(() => {
+    const $router = {
+        push: jest.fn()
+    }
+    const $route = {
+        path: '/home',
+    }
+
+    navbarWrapper = mount(
+        Navbar, {
+            mocks: {
+                $router,
+                $route
+            },
+            stubs: {RouterLink: RouterLinkStub}
+        });
+    sideNavBarWrapper = mount(
+        SideNavBar,
+        {
+            mocks: {
+                $router,
+                $route
+            },
+            stubs: {RouterLink: RouterLinkStub}
+        });
+    homeWrapper = mount(
+        Home,
+        {
+            mocks: {
+                $router,
+                $route
+            }
+        });
+
+    sideNavToggleButton = navbarWrapper.find("#side-nav-bar-open-btn");
+    sideNavBar = sideNavBarWrapper.find("#side-nav-bar");
+    homePage = homeWrapper.find("main")
+    sideNavCloseButton = sideNavBarWrapper.find("#closebtn");
+
+})
 
 describe("Testing the side navigation buttons' functionality", () => {
 
@@ -65,41 +116,180 @@ describe("Testing the side navigation buttons' functionality", () => {
 
 describe("Testing the side navigation bar opens and closes", () => {
 
-    const navbarWrapper = shallowMount(
-        Navbar);
-    const sideNavBarWrapper = shallowMount(
-        SideNavBar);
-    const homeWrapper = shallowMount(Home);
-
-    //TODO: add in mocking of isActivePath(path)
 
     test('Testing the button on the top navigation bar opens the side navigation bar when the side navigation ' +
         'bar is closed', () => {
-
-        const sideNavToggleButton = navbarWrapper.find("#side-nav-bar-open-btn");
-        const sideNavBar = sideNavBarWrapper.find("#side-nav-bar");
-        const homePage = homeWrapper.find("main")
 
         // Mock clicking the open button
         sideNavToggleButton.trigger("click");
         navbarWrapper.vm.$nextTick();
 
-        sideNavToggleButton.toggleSideNav();
+        // sideNavToggleButton.toggleSideNav();
+        sideNavBarWrapper.vm.toggleSideNav();
 
         expect(sideNavBar.style.width).toBe("250px");
         expect(homePage.style.marginLeft).toBe("250px");
 
     })
 
-    test('Testing the button on the top navigation bar closes the side  navigation bar when the side navigation ' +
+    test('Testing the button on the top navigation bar closes the side navigation bar when the side navigation ' +
         'bar is open', () => {
+
+        // Mock clicking the open button
+        sideNavToggleButton.trigger("click");
+        navbarWrapper.vm.$nextTick();
+
+        // sideNavToggleButton.toggleSideNav();
+        sideNavBarWrapper.vm.toggleSideNav();
+
+        expect(sideNavBar.style.width).toBe("0px");
+        expect(homePage.style.marginLeft).toBe("0px");
 
     })
 
     test('Testing the x button on the side navigation bar closes the side navigation bar when the side navigation ' +
         'bar is open', () => {
 
+        // Mock clicking the open button
+        sideNavCloseButton.trigger("click");
+        navbarWrapper.vm.$nextTick();
+
+        // sideNavToggleButton.toggleSideNav();
+        sideNavBarWrapper.vm.closeSideNav();
+
+        expect(sideNavBar.style.width).toBe("0px");
+        expect(homePage.style.marginLeft).toBe("0px");
+
     })
 
+})
+
+describe("closeSideNav() tests", () => {
+
+    test('Testing closeSideNav() closes the side nav bar in terms of SideNavBar when it is open', () => {
+
+        sideNavBarWrapper.closeSideNav();
+        expect(sideNavBar.style.width).toBe("0px");
+        expect(homePage.style.marginLeft).toBe("0px");
+    })
+
+    test('Testing closeSideNav() closes the side nav bar in terms of Navbar when it is open', () => {
+
+        navbarWrapper.closeSideNav();
+        expect(sideNavBar.style.width).toBe("0px");
+        expect(homePage.style.marginLeft).toBe("0px");
+    })
+
+})
+
+describe("toggleSideNav() tests", () => {
+
+    test('Testing toggleSideNav() closes the side nav bar in terms of SideNavBar when it is open', () => {
+
+        sideNavBar.style.width = "250px";
+        homePage.style.marginLeft = "250px";
+
+        sideNavBarWrapper.toggleSideNav();
+
+        expect(sideNavBar.style.width).toBe("0px");
+        expect(homePage.style.marginLeft).toBe("0px");
+    })
+
+    test('Testing toggleSideNav() closes the side nav bar in terms of Navbar when it is open', () => {
+
+        sideNavBar.style.width = "250px";
+        homePage.style.marginLeft = "250px";
+
+        navbarWrapper.toggleSideNav();
+
+        expect(sideNavBar.style.width).toBe("0px");
+        expect(homePage.style.marginLeft).toBe("0px");
+    })
+
+    test('Testing toggleSideNav() opens the side nav bar in terms of SideNavBar when it is closed', () => {
+
+        sideNavBar.style.width = "0px";
+        homePage.style.marginLeft = "0px";
+
+        sideNavBarWrapper.toggleSideNav();
+
+        expect(sideNavBar.style.width).toBe("250px");
+        expect(homePage.style.marginLeft).toBe("250px");
+    })
+
+    test('Testing toggleSideNav() opens the side nav bar in terms of Navbar when it is closed', () => {
+
+        sideNavBar.style.width = "0px";
+        homePage.style.marginLeft = "0px";
+
+        navbarWrapper.toggleSideNav();
+
+        expect(sideNavBar.style.width).toBe("250px");
+        expect(homePage.style.marginLeft).toBe("250px");
+    })
+
+})
+
+describe("toggleInteractAs() tests in terms of Navbar", () => {
+
+    test('Testing toggleInteractAs() sets the showInteractMenu to false when the showInteractMenu dropdown is true.', () => {
+        navbarWrapper.vm.showInteractMenu = true;
+        navbarWrapper.vm.toggleInteractAs()
+        expect(navbarWrapper.vm.showInteractMenu).toBe(false)
+    })
+
+    test('Testing toggleInteractAs() sets the showInteractMenu to true when the showInteractMenu dropdown is false.', () => {
+        navbarWrapper.vm.showInteractMenu = false;
+        navbarWrapper.vm.toggleInteractAs()
+        expect(navbarWrapper.vm.showInteractMenu).toBe(true)
+    })
+
+})
+
+describe("toggleBusinessDropdown() tests in terms of Navbar", () => {
+
+    test('Testing toggleBusinessDropdown() sets the showBusinessDropdown to false when the showBusiness is true.', () => {
+        navbarWrapper.vm.showBusinessDropdown = true;
+        navbarWrapper.vm.toggleBusinessDropdown()
+        expect(navbarWrapper.vm.showBusinessDropdown).toBe(false)
+    })
+
+    test('Testing toggleBusinessDropdown() sets the showBusinessDropdown to true when the showBusiness is false.', () => {
+        navbarWrapper.vm.showBusinessDropdown = false;
+        navbarWrapper.vm.toggleBusinessDropdown()
+        expect(navbarWrapper.vm.showBusinessDropdown).toBe(true)
+    })
+
+})
+
+describe("toggleInteractAs() tests in terms of SideNavBar", () => {
+
+    test('Testing toggleInteractAs() sets the showInteractMenu to false when the showInteractMenu dropdown is true.', () => {
+        navbarWrapper.vm.showInteractMenu = true;
+        navbarWrapper.vm.toggleInteractAs()
+        expect(navbarWrapper.vm.showInteractMenu).toBe(false)
+    })
+
+    test('Testing toggleInteractAs() sets the showInteractMenu to true when the showInteractMenu dropdown is false.', () => {
+        navbarWrapper.vm.showInteractMenu = false;
+        navbarWrapper.vm.toggleInteractAs()
+        expect(navbarWrapper.vm.showInteractMenu).toBe(true)
+    })
+
+})
+
+describe("toggleBusinessDropdown() tests in terms of SideNavBar", () => {
+
+    test('Testing toggleBusinessDropdown() sets the showBusinessDropdown to false when the showBusiness is true.', () => {
+        navbarWrapper.vm.showBusinessDropdown = true;
+        navbarWrapper.vm.toggleBusinessDropdown()
+        expect(navbarWrapper.vm.showBusinessDropdown).toBe(false)
+    })
+
+    test('Testing toggleBusinessDropdown() sets the showBusinessDropdown to true when the showBusiness is false.', () => {
+        navbarWrapper.vm.showBusinessDropdown = false;
+        navbarWrapper.vm.toggleBusinessDropdown()
+        expect(navbarWrapper.vm.showBusinessDropdown).toBe(true)
+    })
 
 })
