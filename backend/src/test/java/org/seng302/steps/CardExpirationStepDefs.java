f7e315f58a5b1336f37717e6d9bb7250bbecd37a
Zhedong Cao <zca19@uclive.ac.nz>
2021-07-27 13:42:39 +1200

    S302T400-523 #time 1h write 3 cucumber tests for UCM4 AC3 and AC4.

--- backend/src/test/java/org/seng302/steps/CardExpirationStepDefs.java
+++ backend/src/test/java/org/seng302/steps/CardExpirationStepDefs.java
@@ -5,32 +5,30 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import org.seng302.MainApplicationRunner;
 import org.seng302.controller.MarketplaceCardResource;
 import org.seng302.exceptions.IllegalAddressArgumentException;
 import org.seng302.exceptions.IllegalMarketplaceCardArgumentException;
 import org.seng302.exceptions.IllegalUserArgumentException;
 import org.seng302.model.Address;
+import org.seng302.model.MarketCardNotification;
 import org.seng302.model.MarketplaceCard;
 import org.seng302.model.User;
 import org.seng302.model.enums.Role;
 import org.seng302.model.enums.Section;
-import org.seng302.model.repository.KeywordRepository;
-import org.seng302.model.repository.MarketCardNotificationRepository;
-import org.seng302.model.repository.MarketplaceCardRepository;
-import org.seng302.model.repository.UserRepository;
+import org.seng302.model.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import javax.servlet.http.Cookie;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
+import javax.transaction.Transactional;
+import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,7 +36,6 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 public class CardExpirationStepDefs extends CucumberSpringConfiguration {
 
@@ -61,12 +58,33 @@ public class CardExpirationStepDefs extends CucumberSpringConfiguration {
     @MockBean
     private MarketCardNotificationRepository marketCardNotificationRepository;
 
+    @Autowired
+    private UserRepository unMockUserRepository;
+
+    @Autowired
+    private BusinessRepository unMockBusinessRepository;
+
+    @Autowired
+    private AddressRepository unMockAddressRepository;
+
+    @Autowired
+    private ProductRepository unMockProductRepository;
+
+    @Autowired
+    private MarketplaceCardRepository unMockMarketplaceCardRepository;
+
+    @Autowired
+    private MarketCardNotificationRepository unMockMarketCardNotificationRepository;
+
     private MockHttpServletResponse response;
+
+    private MainApplicationRunner mainApplicationRunner;
 
     private User user;
     private User anotherUser;
     private MarketplaceCard card;
     private LocalDateTime initialDisplayPeriodEnd;
+    private String notificationMessage;
 
 
     @Before
@@ -213,7 +231,7 @@ public class CardExpirationStepDefs extends CucumberSpringConfiguration {
     }
 
     @Then("I receive a 200 response and the display period end has been extended by two weeks.")
-    public void iReceiveA200ReponseAndTheDisplayPeriodEndHasBeenExtendedByOneWeek() {
+    public void iReceiveA200ResponseAndTheDisplayPeriodEndHasBeenExtendedByOneWeek() {
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
 
         Optional<MarketplaceCard> marketplaceCardOptional = marketplaceCardRepository.findById(card.getId());
@@ -223,4 +241,113 @@ public class CardExpirationStepDefs extends CucumberSpringConfiguration {
         Assertions.assertEquals(initialDisplayPeriodEnd.plusWeeks(2), marketplaceCard.getDisplayPeriodEnd());
     }
 
+    @When("I delete a card.")
+    public void i_delete_a_card() throws Exception {
+        response = cardMVC.perform(delete(String.format("/cards/%d", card.getId()))
+                .cookie(new Cookie("JSESSIONID", user.getSessionUUID())))
+                .andReturn().getResponse();
+        when(marketplaceCardRepository.findById(card.getId())).thenReturn(Optional.empty());
+    }
+
+    @Then("I receive a 200 response and the card has been deleted.")
+    public void i_receive_a_response_and_the_card_has_been_deleted() {
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        Optional<MarketplaceCard> marketplaceCardOptional = marketplaceCardRepository.findById(card.getId());
+        Assertions.assertTrue(marketplaceCardOptional.isEmpty());
+    }
+
+    @Given("A card has been in the maximum display period.")
+    public void a_card_has_been_in_the_maximum_display_period() throws Exception {
+        mainApplicationRunner = new MainApplicationRunner(
+                unMockUserRepository,
+                unMockBusinessRepository,
+                unMockAddressRepository,
+                unMockProductRepository,
+                unMockMarketplaceCardRepository,
+                unMockMarketCardNotificationRepository);
+
+        notificationMessage = "Your card (Hayley's Birthday) has been expired 0h 0m 1s ago.";
+        Address address = new Address(
+                "3/24",
+                "Ilam Road",
+                "Christchurch",
+                "Canterbury",
+                "New Zealand",
+                "90210",
+                "Ilam"
+        );
+        unMockAddressRepository.save(address);
+
+        user = new User("Bob",
+                "Smith",
+                "Ben",
+                "Bobby",
+                "cool person",
+                "email@email.com",
+                LocalDate.of(2020, 2, 2).minusYears(13),
+                "0271316",
+                address,
+                "Password123!",
+                LocalDateTime.of(LocalDate.of(2021, 2, 2),
+                        LocalTime.of(0, 0)),
+                Role.USER);
+        user.setId(1);
+        user.setSessionUUID(User.generateSessionUUID());
+        unMockUserRepository.save(user);
+
+        Assertions.assertTrue(unMockUserRepository.findBySessionUUID(user.getSessionUUID()).isPresent());
+
+        card = new MarketplaceCard(
+                user.getId(),
+                user,
+                Section.FORSALE,
+                LocalDateTime.now().minusWeeks(2),
+                "Hayley's Birthday",
+                "Come join Hayley and help her celebrate her birthday!"
+        );
+        card.setId(1);
+        unMockMarketplaceCardRepository.save(card);
+
+        Assertions.assertTrue(unMockMarketplaceCardRepository.findById(card.getId()).isPresent());
+        Assertions.assertTrue(unMockMarketplaceCardRepository.findById(card.getId()).get().getDisplayPeriodEnd()
+                .isBefore(LocalDateTime.now()));
+    }
+
+    @Transactional
+    @When("check function been called.")
+    public void check_function_been_called() {
+        mainApplicationRunner.checkNotifications();
+    }
+
+    @Then("I will receive a notification for the expired.")
+    public void i_will_receive_a_notification_for_the_expired() {
+        Assertions.assertTrue(unMockMarketCardNotificationRepository
+                .findByUserIdAndMarketCardId(user.getId(), card.getId()).isPresent());
+        Assertions.assertEquals(notificationMessage, unMockMarketCardNotificationRepository
+                .findByUserIdAndMarketCardId(user.getId(), card.getId()).get().getDescription());
+    }
+
+    @Given("I have no action within 24h for an expired card")
+    public void i_have_no_action_within_24h_for_an_expired_card() {
+        notificationMessage = "Your card (Hayley's Birthday) has been deleted.";
+        LocalDateTime originalTime = card.getDisplayPeriodEnd();
+        card.setDisplayPeriodEnd(originalTime.minusHours(24));
+        unMockMarketplaceCardRepository.save(card);
+    }
+
+    @Then("The card will been deleted")
+    public void the_card_will_been_deleted() {
+        Assertions.assertTrue(unMockMarketplaceCardRepository.findById(card.getId()).isEmpty());
+    }
+
+    @Then("I will receive a notification for the deletion.")
+    public void i_will_receive_a_notification_for_the_deletion() {
+        List<MarketCardNotification> marketCardNotifications = unMockMarketCardNotificationRepository.findAllByUserId(user.getId());
+        List<String> descriptions = new ArrayList<>();
+        for (MarketCardNotification marketCardNotification : marketCardNotifications) {
+            descriptions.add(marketCardNotification.getDescription());
+        }
+        Assertions.assertTrue(descriptions.contains(notificationMessage));
+    }
 }
