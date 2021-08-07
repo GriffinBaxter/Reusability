3de795a9355fe352e7ec667baa7929aa5f9f2b5d
Jack Patterson <jlp89@uclive.ac.nz>
2021-08-07 18:21:54 +1200

    S302T400-634 #time 2h 13m Created the template for browser sale listing cards and styled the layout of multiple cards using sample data. ?implement ?pair[jlp89, baj40]

---
+++ frontend/src/components/listing/BrowseListingCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <div class="row">
+      <div class="col">
+        <img :src="require('../../../public/test.jpg')" class="card-img-top" alt="default-image">
+      </div>
+      <div class="col p-0">
+          <div class="row">
+            <div class="card-body" id="price-div">
+              <h3>$2.99</h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="card-body">
+              <a class="btn btn-primary green-button" id="seller-info-button">Seller Info</a>
+            </div>
+          </div>
+        </div>
+
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="card-body pb-0 py-1">
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item pb-1 "><h5>Apple (Green) x2</h5></li>
+          <li class="list-group-item">Closing Date: 2nd Sep 2021</li>
+          <li class="list-group-item">Expires: 10th Nov 2021</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BrowseListingCard"
+}
+</script>
+
+<style scoped>
+
+#seller-info-button {
+  width: 82%;
+}
+
+/*#price-div {*/
+/*  margin-left: 1.3em;*/
+/*}*/
+
+.card {
+  margin: 0.5em 0.5em 0.5em 0.5em;
+
+}
+
+.card:hover {
+  cursor: pointer;
+  box-shadow: 2px 2px lawngreen;
+}
+
+
+</style>
--- frontend/src/views/BrowseListings.vue
+++ frontend/src/views/BrowseListings.vue
@@ -3,6 +3,35 @@
     <div id="main">
       <!-- Navbar -->
       <Navbar/>
+      <div class="container mt-5">
+<!--        <input class="form-control" type="text">-->
+        <div class="row pb-5 mb-4">
+          <div class="col-md-5 col-xl-4 mb-4 mb-lg-0 d-flex justify-content-center">
+            <BrowseListingCard></BrowseListingCard>
+          </div>
+          <div class="col-md-5 col-xl-4 mb-4 mb-lg-0 d-flex justify-content-center">
+            <BrowseListingCard></BrowseListingCard>
+          </div>
+          <div class="col-md-5 col-xl-4 mb-4 mb-lg-0 d-flex justify-content-center">
+            <BrowseListingCard></BrowseListingCard>
+          </div>
+          <div class="col-md-5 col-xl-4 mb-4 mb-lg-0 d-flex justify-content-center">
+            <BrowseListingCard></BrowseListingCard>
+          </div>
+          <div class="col-md-5 col-xl-4 mb-4 mb-lg-0 d-flex justify-content-center">
+            <BrowseListingCard></BrowseListingCard>
+          </div>
+          <div class="col-md-5 col-xl-4 mb-4 mb-lg-0 d-flex justify-content-center">
+            <BrowseListingCard></BrowseListingCard>
+          </div>
+          <div class="col-md-5 col-xl-4 mb-4 mb-lg-0 d-flex justify-content-center">
+            <BrowseListingCard></BrowseListingCard>
+          </div>
+          <div class="col-md-5 col-xl-4 mb-4 mb-lg-0 d-flex justify-content-center">
+            <BrowseListingCard></BrowseListingCard>
+          </div>
+        </div>
+      </div>
 
     </div>
     <!-- Footer -->
@@ -13,10 +42,11 @@
 <script>
 import Navbar from "../components/main/Navbar";
 import Footer from "../components/main/Footer";
+import BrowseListingCard from "../components/listing/BrowseListingCard";
 
 export default {
   name: "Listings",
-  components: {Footer, Navbar},
+  components: {Footer, Navbar, BrowseListingCard},
   data() {
     return {}
   },
