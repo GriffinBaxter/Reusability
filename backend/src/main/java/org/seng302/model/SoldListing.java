c8390309b958b073f6686a4d8d14c44649517c74
Troy <tpt38@uclive.ac.nz>
2021-08-09 12:51:43 +1200

    S302T400-597 #time 50m Added new Exception for SoldListings, added validation to SoldListing on creation and created tests for findAllByBusinessId method ?test

--- backend/src/main/java/org/seng302/model/SoldListing.java
+++ backend/src/main/java/org/seng302/model/SoldListing.java
@@ -2,9 +2,12 @@ package org.seng302.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.seng302.exceptions.IllegalSoldListingArgumentException;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 /**
  * Class for Sold listings
@@ -18,6 +21,7 @@ import java.util.Date;
 public class SoldListing {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // autoincrement the ID
     @Column(name = "id", nullable = false)
     private Integer id;
 
@@ -30,10 +34,10 @@ public class SoldListing {
     private User customer;
 
     @Column(name = "saleDate", nullable = false)
-    private Date saleDate;
+    private LocalDateTime saleDate;
 
     @Column(name = "listingDate", nullable = false)
-    private Date listingDate;
+    private LocalDateTime listingDate;
 
     @JoinColumn(name = "productId", nullable = false)
     private ProductId productId;
@@ -46,4 +50,53 @@ public class SoldListing {
 
     @Column(name = "bookmarks", nullable = false)
     private Integer bookmarks;
+
+    private static final Logger logger = LogManager.getLogger(SoldListing.class.getName());
+
+    public SoldListing(Business business, User customer, LocalDateTime listingDate, ProductId productId, Integer quantity, Double price, Integer bookmarks) throws IllegalSoldListingArgumentException {
+        if (business != null) {
+            this.business = business;
+        } else {
+            logger.error("Sold Listing Creation Error - Business is null");
+            throw new IllegalSoldListingArgumentException("Invalid business");
+        }
+        if (customer != null) {
+            this.customer = customer;
+        } else {
+            logger.error("Sold Listing Creation Error - Customer is null");
+            throw new IllegalSoldListingArgumentException("Invalid customer");
+        }
+        if (listingDate != null && listingDate.isBefore(LocalDateTime.now())) {
+            this.listingDate = listingDate;
+        } else {
+            logger.error("Sold Listing Creation Error - listingDate is null or after saleDate");
+            throw new IllegalSoldListingArgumentException("Invalid listingDate");
+        }
+        if (productId != null) {
+            this.productId = productId;
+        } else {
+            logger.error("Sold Listing Creation Error - productId is null");
+            throw new IllegalSoldListingArgumentException("Invalid productId");
+        }
+        if (quantity != null && quantity >= 1) {
+            this.quantity = quantity;
+        } else {
+            logger.error("Sold Listing Creation Error - quantity is null or less than 1");
+            throw new IllegalSoldListingArgumentException("Invalid quantity");
+        }
+        if (price != null && price > 0) {
+            this.price = price;
+        } else {
+            logger.error("Sold Listing Creation Error - price is null or less than 0");
+            throw new IllegalSoldListingArgumentException("Invalid price");
+        }
+        if (bookmarks != null && bookmarks >= 0) {
+            this.bookmarks = bookmarks;
+        } else {
+            logger.error("Sold Listing Creation Error - bookmarks is null or less than 0");
+            throw new IllegalSoldListingArgumentException("Invalid bookmarks");
+        }
+
+        this.saleDate = LocalDateTime.now();
+    }
 }
---
+++ backend/src/test/java/org/seng302/business/soldListing/SoldListingRepositoryIntegrationTests.java
@@ -0,0 +1,227 @@
+package org.seng302.business.soldListing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.seng302.Main;
+import org.seng302.model.*;
+import org.seng302.model.enums.BusinessType;
+import org.seng302.model.enums.Role;
+import org.seng302.model.repository.SoldListingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ContextConfiguration(classes = {Main.class})
+@ActiveProfiles("test")
+public class SoldListingRepositoryIntegrationTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private SoldListingRepository soldListingRepository;
+
+    private Address address;
+
+    private User user;
+
+    private Business business;
+    private Integer businessId;
+    private Business anotherBusiness;
+    private Integer anotherBusinessId;
+
+    private Product product;
+    private Product anotherProduct;
+
+    private InventoryItem inventoryItem;
+
+    private Listing listing;
+
+    private SoldListing soldListing;
+    private SoldListing anotherSoldListing;
+
+    /**
+     * Creates and inserts all entities needed for testing.
+     * @exception Exception Any exception.
+     */
+    @BeforeEach
+    void setup() throws Exception {
+        // Given
+        address = new Address(
+                "3/24",
+                "Ilam Road",
+                "Christchurch",
+                "Canterbury",
+                "New Zealand",
+                "90210",
+                "Ilam"
+        );
+        entityManager.persist(address);
+        entityManager.flush();
+        user = new User(
+                "first",
+                "last",
+                "middle",
+                "nick",
+                "bio",
+                "test@example.com",
+                LocalDate.of(2021, Month.JANUARY, 1).minusYears(13),
+                "123456789",
+                address,
+                "Password123!",
+                LocalDateTime.of(LocalDate.of(2021, Month.JANUARY, 1), LocalTime.of(0, 0)),
+                Role.USER
+        );
+        user = entityManager.persist(user);
+        entityManager.flush();
+        business = new Business(
+                user.getId(),
+                "example name",
+                "some text",
+                address,
+                BusinessType.RETAIL_TRADE,
+                LocalDateTime.now(),
+                user
+        );
+        business = entityManager.persist(business);
+        businessId = business.getId();
+        entityManager.flush();
+
+        anotherBusiness = new Business(
+                user.getId(),
+                "example name 2",
+                "some text 2",
+                address,
+                BusinessType.RETAIL_TRADE,
+                LocalDateTime.now(),
+                user
+        );
+        anotherBusiness = entityManager.persist(anotherBusiness);
+        anotherBusinessId = anotherBusiness.getId();
+        entityManager.flush();
+
+        product = new Product(
+                "APPLE",
+                business,
+                "Apple",
+                "A Description",
+                "Manufacturer",
+                21.00,
+                "9400547002634"
+        );
+        entityManager.persist(product);
+        entityManager.flush();
+
+        anotherProduct = new Product(
+                "BANANA",
+                business,
+                "Banana",
+                "A Description",
+                "Manufacturer",
+                21.00,
+                "9400547002634"
+        );
+        entityManager.persist(anotherProduct);
+        entityManager.flush();
+
+        inventoryItem = new InventoryItem(product,
+                "APPLE",
+                45,
+                6.5,
+                21.99,
+                LocalDate.of(2020, 4, 25),
+                LocalDate.of(2021, 4, 25),
+                LocalDate.of(2021, 4, 25),
+                LocalDate.of(2021, 12, 25));
+        entityManager.persist(inventoryItem);
+        entityManager.flush();
+
+        listing = new Listing(inventoryItem,
+                5,
+                12.0,
+                "",
+                LocalDateTime.of(LocalDate.of(2020, 8, 1), LocalTime.of(0,0,0)),
+                LocalDateTime.of(LocalDate.of(2022, 10, 1), LocalTime.of(0,0,0)));
+        entityManager.persist(listing);
+        entityManager.flush();
+
+        soldListing = new SoldListing(
+                business,
+                user,
+                LocalDateTime.now().minusDays(3),
+                new ProductId(product.getId(), businessId),
+                listing.getQuantity(),
+                listing.getPrice(),
+                0);
+        entityManager.persist(soldListing);
+        entityManager.flush();
+
+        anotherSoldListing = new SoldListing(
+                business,
+                user,
+                LocalDateTime.now().minusDays(3),
+                new ProductId(anotherProduct.getId(), businessId),
+                listing.getQuantity(),
+                listing.getPrice(),
+                0);
+        entityManager.persist(anotherSoldListing);
+        entityManager.flush();
+    }
+
+    /**
+     * Test findAllByBusinessId when businessId is valid and Sold Listing exists
+     */
+    @Test
+    void testFindAllByBusinessId_whenBusinessIdExistsAndSoldListingsExist() {
+        // When
+        Pageable pageable = PageRequest.of(0, 5, Sort.by(Sort.Order.asc("id")));
+        Page<SoldListing> retrievedSoldListings = soldListingRepository.findAllByBusinessId(businessId, pageable);
+
+        // Then
+        assertThat(retrievedSoldListings.getContent().size()).isNotZero();
+        for (Integer i = 0; i < retrievedSoldListings.getContent().size(); i++) {
+            assertThat(retrievedSoldListings.getContent().get(i).getBusiness().getId()).isEqualTo(this.businessId);
+        }
+    }
+
+    /**
+     * Test findAllByBusinessId when businessId is valid but no Sold Listings exist
+     */
+    @Test
+    void testFindAllByBusinessId_whenBusinessIdExistsButNoSoldListingsExist() {
+        // When
+        Pageable pageable = PageRequest.of(0, 5, Sort.by(Sort.Order.asc("id")));
+        Page<SoldListing> retrievedSoldListings = soldListingRepository.findAllByBusinessId(anotherBusinessId, pageable);
+
+        // Then
+        assertThat(retrievedSoldListings.getContent().size()).isZero();
+    }
+
+    /**
+     * Test findAllByBusinessId when businessId doesn't exist
+     */
+    @Test
+    void testFindAllByBusinessId_whenBusinessIdDoesNotExist() {
+        // When
+        Pageable pageable = PageRequest.of(0, 5, Sort.by(Sort.Order.asc("id")));
+        Page<SoldListing> retrievedSoldListings = soldListingRepository.findAllByBusinessId(99999, pageable);
+
+        // Then
+        assertThat(retrievedSoldListings.getContent().size()).isZero();
+    }
+
+}
