baa7f53ebd439939f457c3d23a59bd8cd79dffac
Hayley Krippner <hek21@uclive.ac.nz>
2021-08-09 13:17:17 +1200

    S302T400-632 #time 1h 30m Added the HTML elements for the ordering, filtering and matching fields and began putting into rows and cols. Next need to fix up logic of a few functions and style the elements more. ?implement

--- frontend/src/views/BrowseListings.vue
+++ frontend/src/views/BrowseListings.vue
@@ -3,13 +3,165 @@
     <div id="main">
       <!-- Navbar -->
       <Navbar/>
-      <div class="container mt-5">
-        <div class="row" id="search-bar-container">
-          <div class="input-group" id="search-inputs" style="alignment: center">
-            <input type="text" id="search-bar" ref="searchInput" class="form-control">
-            <button class="btn green-search-button" id="search-button"><i class="fas fa-search" aria-hidden="true"></i></button>
+
+      <!-- TODO: make this a separate component once finished  -->
+
+      <!--------------------------------------------  Searching, ordering, filtering   -------------------------------->
+
+        <!-- Search Bar  -->
+        <div class="container mt-5">
+          <div class="row" id="search-bar-container">
+            <div class="input-group" id="search-inputs" style="alignment: center">
+              <input type="text" id="search-bar" ref="searchInput" class="form-control">
+              <button class="btn green-search-button" id="search-button"><i class="fas fa-search" aria-hidden="true"></i></button>
+            </div>
           </div>
-        </div>
+
+          <!-- Match Fields -->
+          <div class="row" id="search-filter-ordering-options-container">
+            <div class="col" id="match-fields">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="checkbox-product-name">
+                <label class="form-check-label" for="checkbox-product-name">
+                  Product Name
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="checkbox-seller-location">
+                <label class="form-check-label" for="checkbox-seller-location">
+                  Seller Location
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="checkbox-seller-name">
+                <label class="form-check-label" for="checkbox-seller-name">
+                  Seller Name
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="checkbox-seller-type">
+                <label class="form-check-label" for="checkbox-seller-type">
+                  Seller Type
+                </label>
+              </div>
+            </div>
+
+            <div class="col" id="order-menu">
+
+              <!------------------------------------------ ordering by options menu ------------------------------------------->
+              <div class="row">
+                <p class="fw-normal">Order By</p>
+              </div>
+
+              <div class="row">
+                <div class="btn-group col" role="group">
+
+                  <button type="button" class="btn green-button dropdown-toggle order-by-options-btn"
+                          data-bs-toggle="dropdown" aria-expanded="false">{{ orderByOption }}
+                  </button>
+
+                  <ul class="dropdown-menu gap-2" aria-labelledby="btnGroupDrop1">
+                    <!--order by price-->
+                    <button type="button" class="btn green-button-transparent col-12 order-by-options-btn"
+                            @click="setOrderByOption(true, false, false, false, false, false, false)">
+                      Price Low
+                    </button>
+                    <button type="button" class="btn green-button-transparent col-12 order-by-options-btn"
+                            @click="setOrderByOption(false, true, false, false, false, false, false, false)">
+                      Price High
+                    </button>
+
+                    <!--order by product name-->
+                    <button type="button" class="btn green-button-transparent col-12 order-by-options-btn"
+                            @click="setOrderByOption(false, false, true, false, false, false, false, false)">
+                      Product Name
+                    </button>
+
+                    <!--order by country-->
+                    <button type="button" class="btn green-button-transparent col-12 order-by-options-btn"
+                            @click="setOrderByOption(false, false, false, true, false, false, false, false)">
+                      Country
+                    </button>
+
+                    <!--order by city-->
+                    <button type="button" class="btn green-button-transparent col-12 order-by-options-btn"
+                            @click="setOrderByOption(false, false, false, false, true, false, false, false)">
+                      City
+                    </button>
+
+                    <!--order by expiry date-->
+                    <button type="button" class="btn green-button-transparent col-12 order-by-options-btn"
+                            @click="setOrderByOption(false, false, false, false, false, true, false, false)">
+                      Expiry Date Earliest
+                    </button>
+                    <button type="button" class="btn green-button-transparent col-12 order-by-options-btn"
+                            @click="setOrderByOption(false, false, false, false, false, false, true, false)">
+                      Expiry Date Latest
+                    </button>
+
+                    <!--order by seller name-->
+                    <button type="button" class="btn green-button-transparent col-12 order-by-options-btn"
+                            @click="setOrderByOption(false, false, false, false, false, false, false, true)">
+                      Seller Name
+                    </button>
+                  </ul>
+                </div>
+
+              </div>
+
+              <!--------------------------------------------------------------------------------------------------------->
+
+            </div>
+            <div class="col" id="price-and-date-filters">
+
+              <div class="row">
+                <form>
+                  <div class="form-group">
+                    <label for="lowest-price-input">Price Range $</label>
+                    <input type="number" class="form-control" id="lowest-price-input" placeholder="0.00">
+
+                    <label for="lowest-price-input">to $</label>
+                    <input type="number" class="form-control" id="highest-price-input" placeholder="0.00">
+                  </div>
+                </form>
+              </div>
+
+              <div class="row">
+                <form>
+                  <div class="form-group">
+                    <label for="lowest-price-input">Closing Date</label>
+                    <input type="date" class="form-control" id="start-date">
+
+                    <label for="lowest-price-input">to $</label>
+                    <input type="date" class="form-control" id="end-date">
+                  </div>
+                </form>
+              </div>
+
+              <div class="row">
+
+                <!--------------------------------------- apply filters button -------------------------------------------->
+                <div class="col">
+                  <!--   TODO: add @click event               -->
+                  <button type="button" class="btn btn-md btn-outline-primary green-button">
+                    Apply Filters
+                  </button>
+                </div>
+
+                <!--------------------------------------- clear filters button -------------------------------------------->
+                <div class="col">
+                  <!--   TODO: add @click event               -->
+                  <button type="button" class="btn btn-md btn-outline-primary green-button">
+                    Clear Filters
+                  </button>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        <!----------------------------------------  Searching, ordering, filtering ends ------------------------------->
+
         <br>
         <div class="row pb-5 mb-4">
           <div class="col-md-5 col-xl-4 mb-4 mb-lg-0 d-flex justify-content-center" v-for="listing in listingList" v-bind:key="listing.id">
@@ -45,12 +197,73 @@ export default {
   data() {
     return {
       // Array that stores all retrieved listings
-      listingList: []
+      listingList: [],
+
+      orderByOption: "priceASC",         // default
+      orderBy: this.$route.query["orderBy"] || "priceASC" // gets orderBy from URL or (if not there) sets to default
+
     }
   },
   computed: {
   },
   methods: {
+    /**
+     * Sets the order by option
+     */
+    setOrderByOption(priceLow, priceHigh, productName, country, city, expiryDateEarliest, expiryDateLatest, sellerName) {
+      if (priceLow) {
+        this.orderByOption = "Price Low"
+      } else if (priceHigh) {
+        this.orderByOption = "Price High"
+      } else if (productName) {
+        this.orderByOption = "Product Name"
+      } else if (country) {
+        this.orderByOption = "Country"
+      } else if (city) {
+        this.orderByOption = "City"
+      } else if (expiryDateEarliest) {
+        this.orderByOption = "Expiry Date Earliest"
+      } else if (expiryDateLatest) {
+        this.orderByOption = "Expiry Date Latest"
+      } else if (sellerName) {
+        this.orderByOption = "Seller Name"
+      }
+      this.orderCards();
+    },
+
+    //TODO: fix logic!
+    /**
+     * Builds the order by value that will be sent to the backend to order the cards by
+     */
+    createOrderByParams() {
+      let direction = "ASC"
+      if (this.orderByOption === "Price High" || this.orderByOption === "Expiry Date Latest") {
+        direction = "DESC"
+      }
+
+      let orderByOptionString = this.orderByOption.toLocaleLowerCase();
+      if (this.orderByOption === "Select Order By") {
+        orderByOptionString = "created"
+      }
+
+      return `${orderByOptionString}${direction}`
+    },
+
+    /**
+     * Order the cards
+     */
+    orderCards() {
+      const order = this.createOrderByParams()
+
+      // Checks the orderBy has changed to prevent NavigationDuplicated Errors
+      if (order !== this.orderBy) {
+        this.orderBy = order;
+        this.$parent.$emit("orderedCards", this.orderBy)
+      }
+
+      // now can use this.orderBy to request cards from backend
+
+    },
   },
   async mounted() {
 
