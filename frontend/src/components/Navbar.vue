941c27c7df2fa8ff1f490d7f57197d1565064bd9
Dan Ronen <dro80@uclive.ac.nz>
2021-08-14 16:51:16 +1200

    S302T400-663 #time 3h Struggling along with combining the navbars. Removing the dropdown animation as it seems to be the root of the problems. ?implement

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,48 +1,69 @@
 <template>
   <div id="navbar-wrapper">
     <nav id="navbar">
-      <div id="hamburger-icon-wrapper">
-        <div id="hamburger-icon">
+      <div id="navbar-content">
+        <div id="hamburger-icon-wrapper">
+        <button id="hamburger-icon" @click="toggleNavbar">
           <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
           </svg>
+        </button>
+      </div>
+        <router-link to="/home" tabindex="-1" style="justify-content: center; align-items: center; display: flex">
+          <img src="../../public/logo_only_med.png" alt="Logo" id="logo-image-nav">
+        </router-link>
+        <div>
+          <button id="act-as-wrapper">
+            <img src="../../public/profile_icon_default.png"
+                 class="rounded-circle img-fluid" id="act-as-image" alt="Acting as image"/>
+            <div id="act-as-name">
+              {{actAsOmit}}
+            </div>
+          </button>
         </div>
       </div>
-      <ul id="links-list">
-        <li class="a-nav-item">
-          <router-link to="/" class="router-nav-link">Home</router-link>
+      <ul id="links-list" v-if="showNavbar">
+        <li class="a-nav-item" >
+          <router-link to="/home" class="router-nav-link" active-class="active-link">Home</router-link>
         </li>
 
-        <li class="a-nav-item">
-          <router-link to="/" class="router-nav-link">Profile</router-link>
-          <ul class="is-dropdown">
+        <li class="a-nav-item" v-if="actAsId">
+          <router-link :to="'/businessProfile/' + actAsId" class="router-nav-link" active-class="active-link">Profile</router-link>
+        </li>
+
+        <li class="a-nav-item" v-else>
+          <router-link to="/profile" class="router-nav-link" active-class="active-link">Profile</router-link>
+        </li>
+
+        <li class="a-nav-item" v-if="!isActAsBusiness">
+          <router-link to="/marketplace" class="router-nav-link" active-class="active-link">Marketplace</router-link>
+        </li>
+
+        <li class="a-nav-item" v-if="isActAsBusiness">
+          <button class="router-nav-link add-dropdown-icon" @click="toggleBusinessDropdown">Business Pages</button>
+          <ul id="business-dropdown" class="is-dropdown" v-if="showBusinessDropdown">
             <li class="a-nav-item">
-              <router-link to="/" class="router-nav-link">Marketplace</router-link>
+              <router-link :to="'/businessProfile/' + businessAccountId + '/listings'" class="router-nav-link" active-class="active-link">Listings</router-link>
             </li>
             <li class="a-nav-item">
-              <router-link to="/" class="router-nav-link">Marketplace</router-link>
+              <router-link :to="'/businessProfile/' + businessAccountId + '/inventory'" class="router-nav-link" active-class="active-link">Inventory</router-link>
             </li>
             <li class="a-nav-item">
-              <router-link to="/" class="router-nav-link">Marketplace</router-link>
+              <router-link :to="'/businessProfile/' + businessAccountId + '/productCatalogue'" class="router-nav-link" active-class="active-link">Catalogue</router-link>
             </li>
           </ul>
         </li>
 
 
-        <li class="a-nav-item">
-          <a class="router-nav-link">Business Pages</a>
+        <li class="a-nav-item" >
+          <button @click="logout" class="router-nav-link" active-class="active-link">Logout</button>
         </li>
-
-        <li class="a-nav-item">
-          <router-link to="/" class="router-nav-link">Marketplace</router-link>
-        </li>
-
-        <li class="a-nav-item">
-          <router-link to="/" class="router-nav-link">Logout</router-link>
-        </li>
+      </ul>
+      <ul>
 
       </ul>
     </nav>
+
   </div>
 </template>
 
@@ -66,7 +87,6 @@ export default {
     return {
       // business dropdown variables
       showBusinessDropdown: false,
-
       // Interact as Menu
       showInteractMenu: false,
 
@@ -87,7 +107,7 @@ export default {
       // omit part of name
       showOmitName: null,
       interactAsOmit: [],
-      actAsOmit: "",
+      actAsOmit: "Johnny",
 
       // Watch window width
       screenWidth: document.body.clientWidth,
@@ -110,6 +130,18 @@ export default {
       if (userIdCookie === undefined) {
         await this.logout(new Event("Not logged in"));
       }
+    },
+    /**
+     *
+     */
+    toggleBusinessDropdown() {
+      this.showBusinessDropdown = !this.showBusinessDropdown;
+    },
+    toggleNavbar() {
+      if (this.showNavbar) {
+        this.showBusinessDropdown = false;
+      }
+      this.showNavbar = !this.showNavbar;
     },
     /**
      * Gets information about the current logged in user
@@ -204,10 +236,17 @@ export default {
 
 </script>
 
-<style scoped >
+<style >
 
   #navbar-wrapper {
     height: 6rem;
+
+  }
+
+  @media (min-width: 921px) {
+    #navbar-wrapper {
+      display: none;
+    }
   }
 
   #navbar {
@@ -219,12 +258,25 @@ export default {
     background-color: white;
     padding: 0.5rem 1.75rem;
     box-shadow: 0.4rem 0 1.75rem #00000030;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #navbar-content {
+    padding-top: 1rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
   }
 
   #hamburger-icon-wrapper {
     min-height: 5rem;
     display: flex;
     align-items: center;
+    border: none;
+    outline: none;
+    box-shadow: none;
+    background-color: transparent;
   }
 
   #hamburger-icon {
@@ -249,11 +301,29 @@ export default {
     background-color: #2eda77;
   }
 
+  #hamburger-icon:focus {
+    border: #2eda77 solid 2px;
+    color: white;
+    background-color: #2eda77;
+  }
+
+  #hamburger-icon:focus-visible {
+    border: #2eda77 solid 2px;
+    color: white;
+    background-color: #2eda77;
+  }
+
   #links-list {
     margin: 0;
     list-style: none;
     padding: 0;
+
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    transition: all 3000ms ease;
   }
+
 
   .a-nav-item {
     width: 100%;
@@ -266,6 +336,7 @@ export default {
     background-color: #19b092;
     text-decoration: none;
     transition: 150ms ease-in-out all;
+    border: transparent ;
 
     width: 100%;
     height: 100%;
@@ -279,9 +350,47 @@ export default {
     padding: 0.5em;
   }
 
+  .active-link {
+    color: #fff;
+    background-color: #ef5e33;
+    border: none;
+    outline: none;
+    box-shadow: none;
+  }
+
   .router-nav-link:hover {
     color: #fff;
     background-color: #ef5e33;
+    border: none;
+    outline: none;
+    box-shadow: none;
+  }
+
+  .router-nav-link:focus {
+    color: #fff;
+    background-color: #ef5e33;
+    border: none;
+    outline: none;
+    box-shadow: none;
+  }
+
+  .router-nav-link:focus-visible {
+    color: #fff;
+    background-color: #ef5e33;
+    border: none;
+    outline: none;
+    box-shadow: none;
+  }
+
+  .add-dropdown-icon::after {
+    display: inline-block;
+    margin-left: 0.255em;
+    vertical-align: 0.255em;
+    content: "";
+    border-top: .3em solid;
+    border-right: .3em solid transparent;
+    border-bottom: 0;
+    border-left: .3em solid transparent;
   }
 
   .is-dropdown {
@@ -299,5 +408,44 @@ export default {
     flex-direction: column;
   }
 
+  #logo-image-nav {
+    max-width: 90px;
+    width: 100%;
+  }
+
+  #act-as-wrapper {
+    background-color: transparent;
+    border: none;
+
+    height: 100%;
+    width: 100%;
+    cursor: pointer;
+    transition: all 300ms ease-in-out;
+    outline: transparent;
+  }
+
+  #act-as-image {
+    border: 1px solid #d3d3d3;
+    height: 55px;
+    width: auto;
+  }
+
+  #act-as-wrapper:hover {
+    outline: solid 1px #1eba8c;
+    border: none;
+    box-shadow: none;
+  }
+
+  #act-as-wrapper:focus {
+    outline: solid 1px #1eba8c;
+    border: none;
+    box-shadow: none;
+  }
+
+  #act-as-wrapper:focus-visible {
+    border: none;
+    outline: solid 1px #1eba8c;
+    box-shadow: none;
+  }
 
 </style>
