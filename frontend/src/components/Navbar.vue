344e9bb23e4e5961df2ff9bb16a16236072aa2d8
dro80 <dro80@uclive.ac.nz>
2021-08-18 15:11:36 +1200

    S302T400-666 #time 5m Merged dev into branch and resolved the merge conflicts ?fix

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -340,19 +340,30 @@ export default {
     /**
      * update image for bell
      */
-    updateNotificationState() {console.log('ASDAS')
-      Api.getNotifications()
-          .then(response => {
-            this.newNotification = (response.data.length > 0);
-          })
+    updateNotificationState() {
+      if (Cookies.get('actAs') === undefined) {
+        Api.getNotifications()
+          .then(response => this.newNotification = (response.data.length > 0))
           .catch((error) => {
-            if (error.status === 401) {
+            if (error.response.status === 401) {
               // Missing or invalid token
               this.$router.push({path: '/invalidtoken'});
             } else {
               console.log(error)
             }
           });
+      } else {
+      Api.getBusinessNotifications(Cookies.get('actAs'))
+          .then(response => this.newNotification = (response.data.length > 0))
+          .catch((error) => {
+            if (error.response.status === 401) {
+              // Missing or invalid token
+              this.$router.push({path: '/invalidtoken'});
+            } else {
+              console.log(error)
+            }
+          });
+      }
     },
 
     /**
