a4483c0b5e4fecf5cf08f376caa7521a1e74816d
Griffin Baxter <grb96@uclive.ac.nz>
2021-08-29 20:49:32 +1200

    S302T400-744 #time 3m Documented the attributes of the Conversation entity. ?document

--- backend/src/main/java/org/seng302/model/Conversation.java
+++ backend/src/main/java/org/seng302/model/Conversation.java
@@ -6,28 +6,46 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+/**
+ * Conversation Entity (contains messages)
+ */
 @Data
 @NoArgsConstructor
 @Entity
 public class Conversation {
 
+    /**
+     * ID field for the Conversation entity
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "conversation_id", nullable = false)
     private int id;
 
+    /**
+     * User that started the conversation (from a card), lazily fetched
+     */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "instigator_id", nullable = false)
     private User instigator;
 
+    /**
+     * User that created the card, lazily fetched
+     */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;
 
+    /**
+     * The marketplace card of the conversation
+     */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "marketplace_card_id", nullable = false)
     private MarketplaceCard marketplaceCard;
 
+    /**
+     * Date and time of the conversation creation
+     */
     @Column(name = "created", nullable = false)
     private LocalDateTime created;
 
