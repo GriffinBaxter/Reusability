f495042c158035ed24e5282cf5248061df884ca9
Zhedong Cao <zca19@uclive.ac.nz>
2021-08-30 15:07:11 +1200

    S302T400-741 #time 15m implement radios for select search type. ?implement

--- frontend/src/components/productCatalogue/ProductSearchBar.vue
+++ frontend/src/components/productCatalogue/ProductSearchBar.vue
@@ -7,6 +7,29 @@
       </button>
     </div>
 
+    <div class="form-check form-check-inline" id="search-by-product-name">
+      <input class="form-check-input" type="radio" name="search-type-radio" id="radio-product-name"
+             value="PRODUCT_NAME" checked>
+      <label class="form-check-label" for="radio-product-name">Product Name</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="search-type-radio" id="radio-product-id" value="PRODUCT_ID">
+      <label class="form-check-label" for="radio-product-id">Product ID</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="search-type-radio" id="radio-manufacturer"
+             value="MANUFACTURER">
+      <label class="form-check-label" for="radio-manufacturer">Manufacturer</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="search-type-radio" id="radio-description"
+             value="DESCRIPTION">
+      <label class="form-check-label" for="radio-description">Description</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="search-type-radio" id="radio-all" value="ALL">
+      <label class="form-check-label" for="radio-all">ALL</label>
+    </div>
 
   </div>
 
@@ -16,12 +39,22 @@
 export default {
   name: "ProductSearchBar",
   data() {
-    return {
-
-    }
+    return {}
   },
   methods: {
+    /**
+     * Finds the selected radio button
+     * @return The value of the selected radio button
+     */
+    getSelectedRadio() {
+      let radios = document.querySelectorAll("input[name='search-type-radio']");
 
+      for (const radio of radios) {
+        if (radio.checked) {
+          return radio.value;
+        }
+      }
+    },
     /**
      * When the enter key is pressed, the query is run with the search value.
      */
