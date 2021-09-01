7226f0ef189ae7dc3df5543a1daaae1f8d566085
dro80 <dro80@uclive.ac.nz>
2021-09-01 22:26:44 +1200

    S302T400-729 #time 27m Added an API call for the backend to get the conversations ?implement

--- frontend/src/components/messages/Messages.vue
+++ frontend/src/components/messages/Messages.vue
@@ -1,7 +1,12 @@
 <template>
-  <div id="messages-wrapper">
-    <div id="content-wrapper">
-      <MessageOption v-for="(msg, i) in messages" :key="i" :username="msg.username" :new-message="msg.newMessage" :card-name="msg.cardName"></MessageOption>
+  <div id="component-wrapper">
+    <div id="messages-wrapper">
+      <div id="content-wrapper">
+        <MessageOption v-for="(conv) in conversations" :key="conv.id" :username="conv.username" :new-message="conv.newMessage" :card-name="conv.cardName"></MessageOption>
+      </div>
+    </div>
+    <div class="error-message" v-if="errorMessage">
+      {{errorMessage}}
     </div>
   </div>
 </template>
@@ -15,7 +20,7 @@ export default {
   components: {MessageOption},
   data() {
     return {
-      messages: [
+      conversations: [
         {
           username: "Pontiff Sulyvahn",
           cardName: "Card name Example",
@@ -70,23 +75,44 @@ export default {
       errorMessage: ""
     }
   },
+  methods: {
+    toastErrorMessage(message) {
+      this.errorMessage = message;
+      setTimeout(() => {
+        this.errorMessage = "";
+      }, 1000)
+    }
+  },
   beforeMount() {
     this.errorMessage = "";
     Api.getConversations().then(
         (res) => {
-          this.messages = res.data.map( () => {})
+          this.conversations = res.data.map( (conversation) => {
+            return {
+              id: conversation.id,
+              image: conversation.instigatorImage && "../../../public/profile_icon_default.png",
+              userName: conversation.instigatorName,
+              cardName: conversation.marketplaceCardTitle,
+              creation_time: conversation.created,
+              newMessage: true
+            };
+          });
+
+          if (this.conversations.length === 0) {
+            this.errorMessage = "No messages found.";
+          }
         }
     ).catch((err) => {
       if (err.response) {
         if (err.response.status === 401) {
           this.$router.push({path: '/invalidtoken'});
         } else  {
-          this.errorMessage = "Something went wrong sorry."
+          this.toastErrorMessage("Something went wrong sorry.");
         }
       } else if (err.request) {
-          this.errorMessage = "Timed out getting conversations."
+          this.toastErrorMessage("Timed out getting conversations.")
       } else {
-        this.errorMessage = "Something went wrong sorry."
+        this.toastErrorMessage("Something went wrong sorry.");
       }
     });
   }
@@ -96,9 +122,18 @@ export default {
 
 <style scoped>
 
-  #messages-wrapper {
+  #component-wrapper {
     position: absolute;
     top: 100%;
+    right: 15px;
+    z-index: 999;
+    width: 100%;
+    height: max(400px, 100%);
+  }
+
+  #messages-wrapper {
+    position: absolute;
+    top: 0;
     right: 15px;
     z-index: 999;
 
@@ -112,6 +147,8 @@ export default {
     background-color: white;
   }
 
+
+
   #content-wrapper {
     max-width: 270px;
     height: fit-content;
@@ -120,5 +157,17 @@ export default {
     grid-template-columns: 1fr;
   }
 
+  .error-message {
+    position: absolute;
+    z-index: 1000;
+    top: 0;
+    text-align: center;
+    right: 15px;
+    max-width: 274px;
+    width: 100%;
+    height: 24px;
+
+    background-color: #f8080850;
+  }
 
 </style>
