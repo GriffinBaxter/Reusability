0bb6e3c4bc8b9af7bd3234697e3fd1b1ae245103
Billie Johnson <baj40@uclive.ac.nz>
2021-09-02 18:52:51 +1200

    Added log

--- backend/src/main/java/org/seng302/controller/MarketplaceConversationResource.java
+++ backend/src/main/java/org/seng302/controller/MarketplaceConversationResource.java
@@ -184,6 +184,7 @@ public class MarketplaceConversationResource {
         User currentUser = Authorization.getUserVerifySession(sessionToken, userRepository);
 
         List<Conversation> conversationList = marketplaceConversationRepository.findAllByInstigatorIdOrReceiverId_OrderByCreatedDesc(currentUser.getId(), currentUser.getId());
+        logger.info("Conversations retrieved user with ID {}", currentUser.getId());
 
         return toConversationPayloadList(conversationList);
     }
