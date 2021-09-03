63a61eedf830ba2040f3add5cded9b3ece7acc7c
Zhedong Cao <zca19@uclive.ac.nz>
2021-09-03 12:18:19 +1200

    S302T400-775 #time 50m write cucumber tests for U10 AC5, AC6, AC7. ?test

--- backend/src/test/java/org/seng302/steps/ModifyUserStepDefs.java
+++ backend/src/test/java/org/seng302/steps/ModifyUserStepDefs.java
@@ -4,43 +4,79 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.mockito.Mockito;
+import org.seng302.controller.ImageResource;
 import org.seng302.controller.UserResource;
 import org.seng302.exceptions.IllegalAddressArgumentException;
 import org.seng302.exceptions.IllegalUserArgumentException;
 import org.seng302.model.Address;
 import org.seng302.model.User;
+import org.seng302.model.UserImage;
 import org.seng302.model.enums.Role;
-import org.seng302.model.repository.AddressRepository;
-import org.seng302.model.repository.UserRepository;
+import org.seng302.model.repository.*;
+import org.seng302.services.FileStorageService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.Cookie;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 /**
  * Modify user profile step definitions class
  */
 public class ModifyUserStepDefs {
-    private MockMvc mvc;
+
+    private MockMvc userMvc;
+
+    private MockMvc imageMvc;
 
     @MockBean
     private UserRepository userRepository;
 
     @MockBean
     private AddressRepository addressRepository;
+
+    @MockBean
+    private BusinessRepository businessRepository;
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    @MockBean
+    private ImageRepository imageRepository;
+
+    @MockBean
+    private UserImageRepository userImageRepository;
+
+    private MockMultipartFile image;
+
+    private FileStorageService fileStorageService;
+
+    private List<UserImage> userImages;
 
     private final String modifiedUserPayload = "{\n" +
             "\"firstName\": \"%s\",\n" +
@@ -76,11 +112,23 @@ public class ModifyUserStepDefs {
 
     private String password;
 
+    private UserImage primaryUserImage;
+
+    private Integer userId;
+
     @Before
     public void createMockMvc() {
         userRepository = mock(UserRepository.class);
         addressRepository = mock(AddressRepository.class);
-        this.mvc = MockMvcBuilders.standaloneSetup(new UserResource(userRepository, addressRepository)).build();
+        businessRepository = mock(BusinessRepository.class);
+        productRepository = mock(ProductRepository.class);
+        imageRepository = mock(ImageRepository.class);
+        userImageRepository = mock(UserImageRepository.class);
+        fileStorageService = Mockito.mock(FileStorageService.class, withSettings().stubOnly());
+
+        this.userMvc = MockMvcBuilders.standaloneSetup(new UserResource(userRepository, addressRepository)).build();
+        this.imageMvc = MockMvcBuilders.standaloneSetup(new ImageResource(businessRepository, userRepository,
+                productRepository, imageRepository, userImageRepository, fileStorageService)).build();
     }
 
 
@@ -130,6 +178,9 @@ public class ModifyUserStepDefs {
         anotherUser.setSessionUUID(User.generateSessionUUID());
 
         cookie = new Cookie("JSESSIONID", user.getSessionUUID());
+        userId = user.getId();
+        primaryUserImage = new UserImage(1, userId, "storage/test", "test/test", true);
+        userImages = new ArrayList<>();
     }
 
     @Given("I have firstname {string}, lastname {string}, middle name {string}, nickname {string}, bio {string},email {string}, date of birth {string}, phone number {string}, password {string}.")
@@ -172,7 +223,7 @@ public class ModifyUserStepDefs {
                 dateOfBirth, phoneNumber, address.getStreetNumber(), address.getStreetName(), address.getSuburb(),
                 address.getCity(), address.getRegion(), address.getCountry(), address.getPostcode(), currentPassword, newPassword);
 
-        response = mvc.perform(put(String.format("/users/%d/profile", user.getId()))
+        response = userMvc.perform(put(String.format("/users/%d/profile", user.getId()))
                 .contentType(MediaType.APPLICATION_JSON).content(payload)
                 .cookie(cookie)).andReturn().getResponse();
     }
@@ -233,7 +284,7 @@ public class ModifyUserStepDefs {
                 user.getPhoneNumber(), streetNumber, streetName, suburb, city, region, country, postcode, password,
                 password);
 
-        response = mvc.perform(put(String.format("/users/%d/profile", user.getId()))
+        response = userMvc.perform(put(String.format("/users/%d/profile", user.getId()))
                 .contentType(MediaType.APPLICATION_JSON).content(payload)
                 .cookie(cookie)).andReturn().getResponse();
     }
@@ -271,7 +322,7 @@ public class ModifyUserStepDefs {
                 user.getPhoneNumber(), address.getStreetNumber(), address.getStreetName(), address.getSuburb(),
                 address.getCity(), address.getRegion(), address.getCountry(), address.getPostcode(), password, password);
 
-        response = mvc.perform(put(String.format("/users/%d/profile", user.getId()))
+        response = userMvc.perform(put(String.format("/users/%d/profile", user.getId()))
                 .contentType(MediaType.APPLICATION_JSON).content(payload)
                 .cookie(cookie)).andReturn().getResponse();
     }
@@ -305,7 +356,7 @@ public class ModifyUserStepDefs {
                 user.getPhoneNumber(), address.getStreetNumber(), address.getStreetName(), address.getSuburb(),
                 address.getCity(), address.getRegion(), address.getCountry(), address.getPostcode(), password, password);
 
-        response = mvc.perform(put(String.format("/users/%d/profile", user.getId()))
+        response = userMvc.perform(put(String.format("/users/%d/profile", user.getId()))
                 .contentType(MediaType.APPLICATION_JSON).content(payload)
                 .cookie(cookie)).andReturn().getResponse();
     }
@@ -325,7 +376,7 @@ public class ModifyUserStepDefs {
                 user.getPhoneNumber(), address.getStreetNumber(), address.getStreetName(), address.getSuburb(),
                 address.getCity(), address.getRegion(), address.getCountry(), address.getPostcode(), password, password);
 
-        response = mvc.perform(put(String.format("/users/%d/profile", user.getId()))
+        response = userMvc.perform(put(String.format("/users/%d/profile", user.getId()))
                 .contentType(MediaType.APPLICATION_JSON).content(payload)
                 .cookie(cookie)).andReturn().getResponse();
     }
@@ -345,7 +396,7 @@ public class ModifyUserStepDefs {
                 address.getStreetNumber(), address.getStreetName(), address.getSuburb(), address.getCity(),
                 address.getRegion(), address.getCountry(), address.getPostcode(), password, password);
 
-        response = mvc.perform(put(String.format("/users/%d/profile", user.getId()))
+        response = userMvc.perform(put(String.format("/users/%d/profile", user.getId()))
                 .contentType(MediaType.APPLICATION_JSON).content(payload)
                 .cookie(cookie)).andReturn().getResponse();
     }
@@ -357,7 +408,7 @@ public class ModifyUserStepDefs {
                 user.getPhoneNumber(), address.getStreetNumber(), address.getStreetName(), address.getSuburb(),
                 address.getCity(), address.getRegion(), address.getCountry(), address.getPostcode(), password, password);
 
-        response = mvc.perform(put(String.format("/users/%d/profile", user.getId()))
+        response = userMvc.perform(put(String.format("/users/%d/profile", user.getId()))
                 .contentType(MediaType.APPLICATION_JSON).content(payload)
                 .cookie(cookie)).andReturn().getResponse();
     }
@@ -377,8 +428,56 @@ public class ModifyUserStepDefs {
                 user.getPhoneNumber(), address.getStreetNumber(), address.getStreetName(), address.getSuburb(),
                 address.getCity(), address.getRegion(), "", address.getPostcode(), password, password);
 
-        response = mvc.perform(put(String.format("/users/%d/profile", user.getId()))
+        response = userMvc.perform(put(String.format("/users/%d/profile", user.getId()))
                 .contentType(MediaType.APPLICATION_JSON).content(payload)
                 .cookie(cookie)).andReturn().getResponse();
     }
+
+    @Given("I have a image with filename of {string}.")
+    public void i_have_a_image_with_filename_of(String fileName) throws IOException {
+        image = new MockMultipartFile("images", fileName,
+                MediaType.IMAGE_JPEG_VALUE, this.getClass().getResourceAsStream(fileName));
+        assertThat(image.getOriginalFilename()).isEqualTo(fileName);
+    }
+
+    @When("I upload this image for myself.")
+    public void i_upload_this_image_for_myself() throws Exception {
+        when(userRepository.findBySessionUUID(user.getSessionUUID())).thenReturn(Optional.of(user));
+        when(userRepository.findById(userId)).thenReturn(Optional.ofNullable(user));
+
+        lenient().when(fileStorageService.generateThumbnail(any(MultipartFile.class), anyString())).thenReturn(
+                new ByteArrayInputStream("mockedThumbnailInputStream".getBytes()));
+        lenient().when(fileStorageService.storeFile(any(InputStream.class), anyString())).thenReturn(true);
+        lenient().when(fileStorageService.getPathString(anyString())).thenReturn(primaryUserImage.getFilename());
+
+        when(userImageRepository.findUserImagesByUserIdAndIsPrimary(userId, true)).thenReturn(userImages);
+        when(userImageRepository.saveAndFlush(any(UserImage.class))).thenReturn(primaryUserImage);
+        response = imageMvc.perform(multipart("/images").file(image).cookie(cookie)
+                        .param("unCheckImageType", "USER_IMAGE")
+                        .param("userId", String.valueOf(user.getId())))
+                .andReturn().getResponse();
+    }
+
+    @Then("This image will be stored.")
+    public void this_image_will_be_stored() throws UnsupportedEncodingException {
+        System.out.println(response.getErrorMessage());
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.getContentAsString()).isEqualTo(String.format("{\"id\":%d}", primaryUserImage.getId()));
+    }
+
+    @Given("There is no image for my account.")
+    public void there_is_no_image_for_my_account() {
+        assertThat(userImages.size()).isZero();
+    }
+
+    @Then("This image will be the primary image.")
+    public void this_image_will_be_the_primary_image() {
+        assertThat(primaryUserImage.getIsPrimary()).isTrue();
+    }
+
+    @Then("A thumbnail of this image will be automatically created.")
+    public void a_thumbnail_of_this_image_will_be_automatically_created() {
+        assertThat(primaryUserImage.getThumbnailFilename()).isNotEmpty();
+    }
+
 }
