20823f4b9d3c417bee42dc11d42af3d449296ccf
Zachary Kaye <zck10@uclive.ac.nz>
2021-09-05 20:09:10 +1200

    S302T400-729 #time 15m Updated the Jest tests for the Message components. ?test
    - Updated the tests for the MessageModal component due to changing the implementation of the API call.
    - Added an extra test for the Message component.

--- frontend/tests/componentTests/messages.test.js
+++ frontend/tests/componentTests/messages.test.js
@@ -10,6 +10,7 @@ import MessageOption from "../../src/components/messages/MessageOption";
 import Api from "../../src/Api";
 import VueLogger from "vuejs-logger";
 import ProfileImage from "../../public/sample_profile_image.jpg"
+import Cookies from "js-cookie";
 
 
 jest.mock("../../src/Api");
@@ -121,16 +122,106 @@ describe("Testing the Messages.vue component", () => {
                 status: 200,
             },
             data: [
-                {id: 1, instigatorName: "Testy", marketplaceCardTitle: "YARRR", created: "2019-2-1"},
-                {id: 2, instigatorName: "Testy", marketplaceCardTitle: "YARRR", created: "2019-2-1"},
-                {id: 3, instigatorName: "Testy", marketplaceCardTitle: "YARRR", created: "2019-2-1"}
+                {
+                    "id": 4,
+                    "instigatorId": 21,
+                    "instigatorName": "John S Doe",
+                    "instigatorImage": null,
+                    "receiverId": 8,
+                    "receiverName": "Frank J Smith",
+                    "receiverImage": null,
+                    "marketplaceCardId": 9,
+                    "marketplaceCardTitle": "Beef Frying",
+                    "created": "2021-09-05T17:22:03.213546"
+                },
+                {
+                    "id": 3,
+                    "instigatorId": 21,
+                    "instigatorName": "John S Doe",
+                    "instigatorImage": null,
+                    "receiverId": 5,
+                    "receiverName": "Minttu Anna Wainio",
+                    "receiverImage": null,
+                    "marketplaceCardId": 7,
+                    "marketplaceCardTitle": "Sealord Fish Fillets Hoki Classic Crumbed",
+                    "created": "2021-09-05T17:20:57.783525"
+                },
+                {
+                    "id": 2,
+                    "instigatorId": 21,
+                    "instigatorName": "John S Doe",
+                    "instigatorImage": null,
+                    "receiverId": 9,
+                    "receiverName": "Lina Jose Mari Patterson",
+                    "receiverImage": null,
+                    "marketplaceCardId": 10,
+                    "marketplaceCardTitle": "Beef Mince Premium",
+                    "created": "2021-09-05T17:09:58.893898"
+                },
+                {
+                    "id": 1,
+                    "instigatorId": 21,
+                    "instigatorName": "John S Doe",
+                    "instigatorImage": null,
+                    "receiverId": 8,
+                    "receiverName": "Frank J Smith",
+                    "receiverImage": null,
+                    "marketplaceCardId": 9,
+                    "marketplaceCardTitle": "Beef Frying",
+                    "created": "2021-09-05T17:09:38.111619"
+                }
             ]
         }
+        Cookies.get = jest.fn().mockImplementation(() => 21);
         Api.getConversations.mockImplementation(() => Promise.resolve(response));
         const wrapper = await factory();
         expect(wrapper.find("#conversation-1").exists()).toBeTruthy();
         expect(wrapper.find("#conversation-2").exists()).toBeTruthy();
         expect(wrapper.find("#conversation-3").exists()).toBeTruthy();
+        expect(wrapper.find("#conversation-4").exists()).toBeTruthy();
+        expect(wrapper.vm.$data.errorMessage).toStrictEqual("");
+        expect(wrapper.find(".error-message").exists()).toBeFalsy();
+    })
+    test("Testing a OK with conversations returned", async () => {
+        const response = {
+            response: {
+                status: 200,
+            },
+            data: [
+                {
+                    "id": 4,
+                    "instigatorId": 21,
+                    "instigatorName": "John S Doe",
+                    "instigatorImage": null,
+                    "receiverId": 8,
+                    "receiverName": "Frank J Smith",
+                    "receiverImage": null,
+                    "marketplaceCardId": 9,
+                    "marketplaceCardTitle": "Beef Frying",
+                    "created": "2021-09-05T17:22:03.213546"
+                },
+                {
+                    "id": 1,
+                    "instigatorId": 21,
+                    "instigatorName": "John S Doe",
+                    "instigatorImage": null,
+                    "receiverId": 8,
+                    "receiverName": "Frank J Smith",
+                    "receiverImage": null,
+                    "marketplaceCardId": 9,
+                    "marketplaceCardTitle": "Beef Frying",
+                    "created": "2021-09-05T17:09:38.111619"
+                }
+            ]
+        }
+        Cookies.get = jest.fn().mockImplementation(() => 8);
+        Api.getConversations.mockImplementation(() => Promise.resolve(response));
+        const wrapper = await factory();
+        // ids (e.g 1) are based on conversation id.
+        expect(wrapper.find("#conversation-1").exists()).toBeTruthy();
+        expect(wrapper.find("#conversation-2").exists()).toBeFalsy();
+        expect(wrapper.find("#conversation-3").exists()).toBeFalsy();
+        expect(wrapper.find("#conversation-4").exists()).toBeTruthy();
         expect(wrapper.vm.$data.errorMessage).toStrictEqual("");
         expect(wrapper.find(".error-message").exists()).toBeFalsy();
     })
