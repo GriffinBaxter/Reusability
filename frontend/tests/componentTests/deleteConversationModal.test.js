838d3eeb687193592ff11de5e8071cb87e700a14
Zachary Kaye <zck10@uclive.ac.nz>
2021-09-08 19:33:57 +1200

    S302T400-732 #time 15m Wrote tests for timeouts in the deleteConversation method ?test

--- frontend/tests/componentTests/deleteConversationModal.test.js
+++ frontend/tests/componentTests/deleteConversationModal.test.js
@@ -5,37 +5,35 @@
 import {shallowMount} from '@vue/test-utils';
 import DeleteConversationModal from "../../src/components/messages/DeleteConversationModal";
 import Api from "../../src/Api";
-import {describe, expect, jest, test} from "@jest/globals";
+import {beforeEach, describe, expect, jest, test} from "@jest/globals";
 
 jest.mock("../../src/Api");
 
-const factory = (values = {}) => {
-    return shallowMount(DeleteConversationModal, {
-        data () {
-            return {
-                ...values
-            }
-        },
-        propsData: {
-            userName: "Rene Rider",
-            id: 2
-        }
-    })
-}
-
 describe("Testing the deleteConversation method.", () => {
+
+    let wrapper;
+    let $router;
+
+    beforeEach(() => {
+        $router = {
+            push: jest.fn()
+        };
+
+        wrapper = shallowMount(
+            DeleteConversationModal,
+            {
+                mocks: {
+                    $router
+                },
+                propsData: {
+                    userName: "Rene Rider",
+                    id: 1
+                }
+            });
+    });
 
     test('Testing an error message is set when a user without permission to delete a conversation' +
         'tries to delete a conversation and receives a 403 error.', async () => {
-        const $router = {
-            push: jest.fn()
-        };
-        const wrapper = await factory({
-            mocks: {
-                $router
-            }
-        });
-
         const data = {
             response: {
                 status: 403
@@ -52,15 +50,6 @@ describe("Testing the deleteConversation method.", () => {
 
     test('Testing an error message is set when a user tries to delete a conversation' +
         'and the frontend receives a 406 error.', async () => {
-        const $router = {
-            push: jest.fn()
-        };
-        const wrapper = await factory({
-            mocks: {
-                $router
-            }
-        });
-
         const data = {
             response: {
                 status: 406
@@ -76,15 +65,6 @@ describe("Testing the deleteConversation method.", () => {
     })
 
     test('Testing a custom event is emitted when deletion is successful.', async () => {
-        const $router = {
-            push: jest.fn()
-        };
-        const wrapper = await factory({
-            mocks: {
-                $router
-            }
-        });
-
         const data = {
             status: 200
         }
@@ -99,15 +79,6 @@ describe("Testing the deleteConversation method.", () => {
 
     test('Testing an error message is set when deletion is successful but an unknown error occurs, such' +
         'as receiving a 204 (No Content) instead of a 200 (OK)', async () => {
-        const $router = {
-            push: jest.fn()
-        };
-        const wrapper = await factory({
-            mocks: {
-                $router
-            }
-        });
-
         const data = {
             status: 204
         }
@@ -122,19 +93,23 @@ describe("Testing the deleteConversation method.", () => {
     })
 
     test('Testing user is redirected to timeout page when a random (500) error occurs', async () => {
-        const $router = {
-            push: jest.fn()
-        };
-        const wrapper = await factory({
-            mocks: {
-                $router
-            }
-        });
-
         const data = {
             response: {
                 status: 500
             }
+        }
+
+        Api.deleteConversation.mockImplementation(() => Promise.reject(data));
+
+        await wrapper.vm.deleteConversation();
+        await wrapper.vm.$nextTick();
+
+        expect($router.push).toHaveBeenCalledWith({path: '/timeout'});
+    })
+
+    test('Testing user is redirected to timeout page when no response is received', async () => {
+        const data = {
+            request:true
         }
 
         Api.deleteConversation.mockImplementation(() => Promise.reject(data));
