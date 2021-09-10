71de8d60ef2256d3129506a3bf0efd3f49676eb4
Griffin Baxter <grb96@uclive.ac.nz>
2021-09-10 20:16:17 +1200

    S302T400-718 #time 18m Created the day selection for the sales report period, which has a minimum of the start of 2021 (as outlined within the API spec, first year of project/sales in-app) and a maximum of the current day. ?implement

--- frontend/src/views/SaleHistory.vue
+++ frontend/src/views/SaleHistory.vue
@@ -25,19 +25,6 @@
         </ul>
       </div>
 
-      <div v-if="period === 'Year'" class="btn-group col d-inline-block p-2" role="group">
-        <button type="button" class="btn green-button dropdown-toggle order-by-options-btn w-100"
-                data-bs-toggle="dropdown" aria-expanded="false">{{ selectedYear }}
-        </button>
-        <ul class="dropdown-menu gap-2" aria-labelledby="btnGroupDrop1">
-          <li class="btn green-button-transparent col-12 order-by-options-btn"
-              v-for="validYear in validYears" v-bind:key="validYear"
-              @click="selectedYear = validYear">
-            {{ validYear }}
-          </li>
-        </ul>
-      </div>
-
       <div v-if="period === 'Month'" class="btn-group col d-inline-block p-2" role="group">
         <button type="button" class="btn green-button dropdown-toggle order-by-options-btn w-100"
                 data-bs-toggle="dropdown" aria-expanded="false">{{ selectedMonth }}
@@ -60,7 +47,7 @@
         </ul>
       </div>
 
-      <div v-if="period === 'Month'" class="btn-group col d-inline-block p-2" role="group">
+      <div v-if="period === 'Year' || period === 'Month'" class="btn-group col d-inline-block p-2" role="group">
         <button type="button" class="btn green-button dropdown-toggle order-by-options-btn w-100"
                 data-bs-toggle="dropdown" aria-expanded="false">{{ selectedYear }}
         </button>
@@ -71,6 +58,10 @@
             {{ validYear }}
           </li>
         </ul>
+      </div>
+
+      <div v-if="period === 'Day'" class="btn-group col d-inline-block p-2" role="group">
+        <input type="date" v-model="selectedDay" :min="'2021-01-01'" :max="currentDay">
       </div>
 
       <div class="container mt-4">
@@ -129,6 +120,7 @@ import Api from "../Api";
 import CurrencyAPI from "../currencyInstance";
 import {formatDate} from "../dateUtils";
 import PageButtons from "../components/PageButtons";
+import {format} from "date-fns";
 
 export default {
   name: "SaleHistory",
@@ -166,16 +158,22 @@ export default {
       totalListings: 0,
 
       // Sales Report Attributes
+
       period: "Year",
+
       currentYear: 0,
-      validYears: [],
       selectedYear: "",
+      validYears: [],
+
+      currentMonth: 0,
+      selectedMonth: "",
       months: [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
       ],
-      currentMonth: 0,
-      selectedMonth: "",
+
+      currentDay: "",
+      selectedDay: "",
     }
   },
   methods: {
@@ -281,9 +279,9 @@ export default {
    * If cookies are invalid or not present, redirect to login page.
    */
   async mounted() {
-    // Sets validYears to a list of years from 2021 to the current year
     const date = new Date();
 
+    // Sets validYears to a list of years from 2021 to the current year
     this.currentYear = date.getFullYear();
     this.selectedYear = this.currentYear;
     this.validYears.push(2021);
@@ -293,6 +291,9 @@ export default {
 
     this.currentMonth = date.getMonth();
     this.selectedMonth = this.months[this.currentMonth];
+
+    this.currentDay = format(date, 'yyyy-MM-dd');
+    this.selectedDay = this.currentDay;
 
     const actAs = Cookies.get('actAs');
     if (checkAccessPermission(this.$route.params.businessId, actAs)) {
