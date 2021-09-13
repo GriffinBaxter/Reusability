753487866081f3965b5f8e7f022c5687690cbe34
Hayley Krippner <hek21@uclive.ac.nz>
2021-09-13 18:54:49 +1200

    S302T400-719 #time 7m Merged the 718 task branch into this branch, resolved merge conflicts and combined the SaleReport from that branch and this branch. Did a sanity check that all the elements were in by looking at the UI. ?chore ?pair[hek21, jlp89]

--- frontend/src/components/saleInsights/SalesReport.vue
+++ frontend/src/components/saleInsights/SalesReport.vue
@@ -5,12 +5,80 @@
         <div class="card p-3">
           <h1 id="page-title">{{ businessName }} Sales Report</h1>
 
+          <!------------------------------------------- Period dropdown menu ------------------------------------------>
+
+          <div class="row m-2">
+            Period:
+            <div class="btn-group col d-inline-block p-2" role="group">
+              <button type="button" class="btn green-button dropdown-toggle order-by-options-btn w-100"
+                      data-bs-toggle="dropdown" aria-expanded="false">{{ period }}
+              </button>
+              <ul class="dropdown-menu gap-2" aria-labelledby="btnGroupDrop1">
+                <li class="btn green-button-transparent col-12 order-by-options-btn"
+                    @click="period = 'Year'">
+                  Year
+                </li>
+                <li class="btn green-button-transparent col-12 order-by-options-btn"
+                    @click="period = 'Month'">
+                  Month
+                </li>
+                <li class="btn green-button-transparent col-12 order-by-options-btn"
+                    @click="period = 'Day'">
+                  Day
+                </li>
+              </ul>
+            </div>
+
+            <div v-if="period === 'Month'" class="btn-group col d-inline-block p-2" role="group">
+              <button type="button" class="btn green-button dropdown-toggle order-by-options-btn w-100"
+                      id="sales-period-select-month"
+                      data-bs-toggle="dropdown" aria-expanded="false">{{ selectedMonth }}
+              </button>
+              <ul class="dropdown-menu gap-2" aria-labelledby="btnGroupDrop1">
+                <div v-if="selectedYear === currentYear">
+                  <li class="btn green-button-transparent col-12 order-by-options-btn"
+                      v-for="month in months.slice(0, this.currentMonth + 1)" v-bind:key="month"
+                      @click="selectedMonth = month">
+                    {{ month }}
+                  </li>
+                </div>
+                <div v-else>
+                  <li class="btn green-button-transparent col-12 order-by-options-btn"
+                      v-for="month in months" v-bind:key="month"
+                      @click="selectedMonth = month">
+                    {{ month }}
+                  </li>
+                </div>
+              </ul>
+            </div>
+
+            <div v-if="period === 'Year' || period === 'Month'" class="btn-group col d-inline-block p-2" role="group">
+              <button type="button" class="btn green-button dropdown-toggle order-by-options-btn w-100"
+                      id="sales-period-select-year"
+                      data-bs-toggle="dropdown" aria-expanded="false">{{ selectedYear }}
+              </button>
+              <ul class="dropdown-menu gap-2" aria-labelledby="btnGroupDrop1">
+                <li class="btn green-button-transparent col-12 order-by-options-btn"
+                    v-for="validYear in validYears" v-bind:key="validYear"
+                    @click="selectedYear = validYear">
+                  {{ validYear }}
+                </li>
+              </ul>
+            </div>
+
+            <div v-if="period === 'Day'" class="btn-group col d-inline-block p-2" role="group">
+              <input type="date" id="sales-period-select-day" v-model="selectedDay" :min="'2021-01-01'" :max="currentDay">
+            </div>
+
+          </div>
+
+
+          <!------------------------------------------- Date inputs --------------------------------------------->
+
           <div class="row m-2">
 
             <form class="needs-validation" novalidate @submit.prevent>
               <div class="form-group" id="date-filtering-container">
-
-                <!------------------------------------------- Date inputs --------------------------------------------->
 
                 <div class="row">
                   <div class="col" style="max-width: 60px">
@@ -102,6 +170,7 @@
 import {isFuture, parseISO} from "date-fns";
 import {isFirstDateBeforeSecondDate} from "../../dateUtils";
 import {toggleInvalidClass} from "../../validationUtils";
+import {format} from "date-fns";
 
 export default {
   name: "SalesReport",
@@ -116,10 +185,31 @@ export default {
   },
   data() {
     return {
+
+      // Sales Report Date Attributes
+
       startDate: null,
       endDate: null,
       invalidDateMsg: "",
-      granularityText: ""
+      granularityText: "",
+
+      // Sales Report Period Attributes
+
+      period: "Year",
+
+      currentYear: 0,
+      selectedYear: "",
+      validYears: [],
+
+      currentMonth: 0,
+      selectedMonth: "",
+      months: [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+      ],
+
+      currentDay: "",
+      selectedDay: "",
     }
   },
   methods: {
@@ -152,7 +242,31 @@ export default {
      */
     setGranularityOption(granularity) {
       this.granularityText = granularity.charAt(0).toUpperCase() + granularity.slice(1);
+    },
+
+    /**
+     * Sets the dates based on the current date (selectable years, months and days).
+     */
+    setDates(date) {
+      // Sets validYears to a list of years from 2021 to the current year
+      this.currentYear = date.getFullYear();
+      this.selectedYear = this.currentYear;
+      this.validYears = [];
+      this.validYears.push(2021);
+      while (this.validYears[this.validYears.length - 1] < this.selectedYear) {
+        this.validYears.push(this.validYears[this.validYears.length - 1] + 1);
+      }
+
+      this.currentMonth = date.getMonth();
+      this.selectedMonth = this.months[this.currentMonth];
+
+      this.currentDay = format(date, 'yyyy-MM-dd');
+      this.selectedDay = this.currentDay;
     }
+  },
+
+  mounted() {
+    this.setDates(new Date());
   }
 }
 </script>
@@ -162,6 +276,12 @@ export default {
 #page-title {
   padding: 10px;
   text-align: center;
+}
+
+.dropdown-menu {
+  margin-top: 0;
+  border: none;
+  width: 200px;
 }
 
 </style>
