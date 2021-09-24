7a4763a281a130411618c013d03fb2434fe97817
Jack Patterson <jlp89@uclive.ac.nz>
2021-09-24 15:35:24 +1200

    S302T400-882 #time 40m Added HTML mail sending method to EmailServiceand added reset link to template and successfully sent HTML email. The endpoint functionality is fully completed now. Took a while to figure out that you need to remove the DocType from the HTML to get it to work. Next I just need to figure out how to properly encode the copyright symbol in the email and thoroughly test the endpoint. ?implement

--- backend/src/main/java/org/seng302/services/EmailService.java
+++ backend/src/main/java/org/seng302/services/EmailService.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Concrete implementation of an email service. Currently sends a basic email with no attachments.
@@ -25,6 +29,16 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+        emailSender.send(message);
+    }
+
+    public void sendHTMLMessage(String to, String subject, String html) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(supportEmail);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(html, true);
         emailSender.send(message);
     }
 
