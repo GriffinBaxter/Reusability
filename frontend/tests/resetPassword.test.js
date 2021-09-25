094067b3d808775479e865d2207e3b1ce9995f27
dro80 <dro80@uclive.ac.nz>
2021-09-25 23:16:52 +1200

    S302T400-879 #time 16m Added tests for seeing if the criteria turns red on focus. ?test

--- frontend/tests/resetPassword.test.js
+++ frontend/tests/resetPassword.test.js
@@ -1,11 +1,11 @@
-import {shallowMount} from "@vue/test-utils";
+import {mount} from "@vue/test-utils";
 import ResetPassword from "../src/views/ResetPassword";
 import {beforeEach, describe, expect, test} from "@jest/globals";
 
 let wrapper;
 
 beforeEach(() => {
-    wrapper = shallowMount(ResetPassword);
+    wrapper = mount(ResetPassword, {attachTo: document.body});
 })
 
 describe("Testing the password field", () => {
@@ -22,29 +22,29 @@ describe("Testing the password field", () => {
         expect(wrapper.vm.$data.password).toStrictEqual("password");
     })
 
-    const toggleShowPassword = (password) => {
+    const toggleShowPassword = async (password) => {
         const showPasswordIcon = wrapper.find("#show-password");
 
         expect(password.element.type).toStrictEqual("password");
 
         showPasswordIcon.trigger("click");
-        wrapper.vm.$nextTick();
+        await wrapper.vm.$nextTick();
         expect(password.element.type).toStrictEqual("text");
 
         showPasswordIcon.trigger("click");
-        wrapper.vm.$nextTick();
+        await wrapper.vm.$nextTick();
         expect(password.element.type).toStrictEqual("password");
 
     }
 
     test("Testing that pressing the show password icon changes the input type for password", async () => {
         const password = await wrapper.find("#password");
-        toggleShowPassword(password);
+        await toggleShowPassword(password);
     })
 
     test("Testing that pressing the show password icon changes the input type confirm password", async () => {
         const password = await wrapper.find("#confirm-password");
-        toggleShowPassword(password);
+        await toggleShowPassword(password);
     })
 })
 
@@ -52,17 +52,22 @@ describe("Testing the dynamic criteria", () => {
 
     const getDyamicCriteria = async (wrapper) => {
         return {
-            lowerCase: await wrapper.find("#"),
-            upperCase: await wrapper.find("#"),
-            number: await wrapper.find("#"),
-            symbol: await wrapper.find("#"),
-            length: await wrapper.find("#")
+            lowerCase: await wrapper.find("#lower-case-criteria"),
+            upperCase: await wrapper.find("#upper-case-criteria"),
+            number: await wrapper.find("#number-criteria"),
+            symbol: await wrapper.find("#symbol-criteria"),
+            length: await wrapper.find("#length-criteria")
         }
     }
 
     test("Testing that on focus all fields turn red", async () => {
 
         const criteria = await getDyamicCriteria(wrapper);
+
+        expect(criteria.lowerCase.classes()).toEqual(["small"])
+        await wrapper.find("#password").trigger("focus")
+        await wrapper.vm.$nextTick();
+        expect(criteria.lowerCase.classes()).toEqual(["small", "text-red"])
     })
 
 })
