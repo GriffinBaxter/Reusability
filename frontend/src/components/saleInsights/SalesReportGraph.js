d583d1fd820496e9012d4c535b02f153ca21df6b
Griffin Baxter <grb96@uclive.ac.nz>
2021-09-27 10:29:40 +1300

    S302T400-811 #time 6m Documented methods within the sales report graph javascript file, and re-formatted the test file which contained an extra nested "describe" that wasn't necessary. ?document

--- frontend/src/components/saleInsights/SalesReportGraph.js
+++ frontend/src/components/saleInsights/SalesReportGraph.js
@@ -1,101 +1,112 @@
-import { Bar } from 'vue-chartjs'
+import {Bar} from 'vue-chartjs'
 
 export default {
-  name: "SalesReportGraph",
-  extends: Bar,
-  props: {
-    labelList: {
-      type: Array,
-      required: true
+    name: "SalesReportGraph",
+    extends: Bar,
+    props: {
+        labelList: {
+            type: Array,
+            required: true
+        },
+        dataList: {
+            type: Array,
+            required: true
+        },
+        sales: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        currencySymbol: {
+            type: String,
+            required: false,
+            default: ""
+        },
+        mock: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
     },
-    dataList: {
-      type: Array,
-      required: true
+    watch: {
+        labelList() {
+            this.renderBarChart(!this.mock);
+        },
+        dataList() {
+            this.renderBarChart(!this.mock);
+        },
     },
-    sales: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-    currencySymbol: {
-      type: String,
-      required: false,
-      default: ""
-    },
-    mock: {
-      type: Boolean,
-      required: false,
-      default: false
-    }
-  },
-  watch: {
-    labelList() {
-      this.renderBarChart(!this.mock);
-    },
-    dataList() {
-      this.renderBarChart(!this.mock);
-    },
-  },
-  computed: {
-    data() {
-      return this.dataList;
-    },
-    labels() {
-      return this.labelList;
-    },
-    label() {
-      if (this.sales) {
-        return "Sales";
-      } else {
-        return "Revenue";
-      }
-    },
-  },
-  methods: {
-    currencyRevenue(value) {
-      return this.currencySymbol + value;
-    },
-    renderBarChart(render) {
-      if (render) {
-        this.renderChart({
-          labels: this.labels,
-          datasets: [
-            {
-              label: this.label,
-              data: this.data,
-              fill: false,
-              borderColor: '#00B88F',
-              backgroundColor: '#00B88F',
-              borderWidth: 1
+    computed: {
+        data() {
+            return this.dataList;
+        },
+        labels() {
+            return this.labelList;
+        },
+        label() {
+            if (this.sales) {
+                return "Sales";
+            } else {
+                return "Revenue";
             }
-          ]
-        }, {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true,
-                callback: this.currencyRevenue
-              },
-              gridLines: {
-                display: true
-              }
-            }],
-            xAxes: [ {
-              gridLines: {
-                display: false
-              }
-            }]
-          },
-          legend: {
-            display: false
-          },
-          responsive: true,
-          maintainAspectRatio: false
-        })
-      }
+        },
+    },
+    methods: {
+        /**
+         * Returns a string concatenating the currency symbol and the currency value for a given y-axis revenue value.
+         *
+         * @param value revenue
+         * @returns {string} currency symbol and value
+         */
+        currencyRevenue(value) {
+            return this.currencySymbol + value;
+        },
+        /**
+         * Renders the bar chart for either sales or revenue.
+         *
+         * @param render Boolean determining whether the bar chart should be rendered.
+         */
+        renderBarChart(render) {
+            if (render) {
+                this.renderChart({
+                    labels: this.labels,
+                    datasets: [
+                        {
+                            label: this.label,
+                            data: this.data,
+                            fill: false,
+                            borderColor: '#00B88F',
+                            backgroundColor: '#00B88F',
+                            borderWidth: 1
+                        }
+                    ]
+                }, {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                callback: this.currencyRevenue
+                            },
+                            gridLines: {
+                                display: true
+                            }
+                        }],
+                        xAxes: [{
+                            gridLines: {
+                                display: false
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false
+                })
+            }
+        }
+    },
+    mounted() {
+        this.renderBarChart(!this.mock);
     }
-  },
-  mounted () {
-    this.renderBarChart(!this.mock);
-  }
 }
--- frontend/tests/componentTests/salesReportGraph.test.js
+++ frontend/tests/componentTests/salesReportGraph.test.js
@@ -14,87 +14,84 @@ describe('Tests the SalesReportGraph component.', () => {
     let wrapper;
     let props;
 
-    describe("", () => {
+    test("Tests that the correct data is populated for a sales graph.", () => {
+        props = {
+            mock: true,  // Custom mock passed through as a prop if active
+            labelList: [
+                'January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021',
+                'June 2021', 'July 2021', 'August 2021', 'September 2021'
+            ],
+            dataList: [0, 0, 0, 2, 0, 1, 0, 0, 1],
+            sales: true,
+        }
 
-        test("Tests that the correct data is populated for a sales graph.", () => {
-            props = {
-                mock: true,  // Custom mock passed through as a prop if active
-                labelList: [
-                    'January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021',
-                    'June 2021', 'July 2021', 'August 2021', 'September 2021'
-                ],
-                dataList: [0, 0, 0, 2, 0, 1, 0, 0, 1],
-                sales: true,
-            }
-
-            wrapper = shallowMount(SalesReportGraph, {
-                propsData: props
-            });
-            
-            expect(wrapper.vm.$props.labelList).toBe(props.labelList);
-            expect(wrapper.vm.$props.dataList).toBe(props.dataList);
+        wrapper = shallowMount(SalesReportGraph, {
+            propsData: props
         });
 
-        test("Tests that the correct data is populated for a revenue graph.", () => {
-            props = {
-                mock: true,  // Custom mock passed through as a prop if active
-                labelList: [
-                    'January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021',
-                    'June 2021', 'July 2021', 'August 2021', 'September 2021'
-                ],
-                dataList: [0, 0, 0, 42, 0, 35, 0, 0, 111],
-                sales: false,
-                currencySymbol: "$"
-            }
+        expect(wrapper.vm.$props.labelList).toBe(props.labelList);
+        expect(wrapper.vm.$props.dataList).toBe(props.dataList);
+    });
 
-            wrapper = shallowMount(SalesReportGraph, {
-                propsData: props
-            });
+    test("Tests that the correct data is populated for a revenue graph.", () => {
+        props = {
+            mock: true,  // Custom mock passed through as a prop if active
+            labelList: [
+                'January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021',
+                'June 2021', 'July 2021', 'August 2021', 'September 2021'
+            ],
+            dataList: [0, 0, 0, 42, 0, 35, 0, 0, 111],
+            sales: false,
+            currencySymbol: "$"
+        }
 
-            expect(wrapper.vm.$props.labelList).toBe(props.labelList);
-            expect(wrapper.vm.$props.dataList).toBe(props.dataList);
-            expect(wrapper.vm.$props.currencySymbol).toBe(props.currencySymbol);
+        wrapper = shallowMount(SalesReportGraph, {
+            propsData: props
         });
 
-        test("Tests that the sales label is returned correctly from the label computed property.", () => {
-            const salesProp = {
-                sales: true
-            }
+        expect(wrapper.vm.$props.labelList).toBe(props.labelList);
+        expect(wrapper.vm.$props.dataList).toBe(props.dataList);
+        expect(wrapper.vm.$props.currencySymbol).toBe(props.currencySymbol);
+    });
 
-            const label = SalesReportGraph.computed.label.call(salesProp);
+    test("Tests that the sales label is returned correctly from the label computed property.", () => {
+        const salesProp = {
+            sales: true
+        }
 
-            expect(label).toBe("Sales");
+        const label = SalesReportGraph.computed.label.call(salesProp);
+
+        expect(label).toBe("Sales");
+    });
+
+    test("Tests that the revenue label is returned correctly from the label computed property.", () => {
+        const salesProp = {
+            sales: false
+        }
+
+        const label = SalesReportGraph.computed.label.call(salesProp);
+
+        expect(label).toBe("Revenue");
+    });
+
+    test("Tests that the currencyRevenue method returns the correct symbol for a given value.", () => {
+        props = {
+            mock: true,  // Custom mock passed through as a prop if active
+            labelList: [
+                'January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021',
+                'June 2021', 'July 2021', 'August 2021', 'September 2021'
+            ],
+            dataList: [0, 0, 0, 42, 0, 35, 0, 0, 111],
+            sales: false,
+            currencySymbol: "$"
+        }
+
+        wrapper = shallowMount(SalesReportGraph, {
+            propsData: props
         });
 
-        test("Tests that the revenue label is returned correctly from the label computed property.", () => {
-            const salesProp = {
-                sales: false
-            }
+        const currencyRevenue = wrapper.vm.currencyRevenue(props.dataList[5]);
 
-            const label = SalesReportGraph.computed.label.call(salesProp);
-
-            expect(label).toBe("Revenue");
-        });
-
-        test("Tests that the currencyRevenue method returns the correct symbol for a given value.", () => {
-            props = {
-                mock: true,  // Custom mock passed through as a prop if active
-                labelList: [
-                    'January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021',
-                    'June 2021', 'July 2021', 'August 2021', 'September 2021'
-                ],
-                dataList: [0, 0, 0, 42, 0, 35, 0, 0, 111],
-                sales: false,
-                currencySymbol: "$"
-            }
-
-            wrapper = shallowMount(SalesReportGraph, {
-                propsData: props
-            });
-
-            const currencyRevenue = wrapper.vm.currencyRevenue(props.dataList[5]);
-
-            expect(currencyRevenue).toBe(props.currencySymbol + props.dataList[5]);
-        });
+        expect(currencyRevenue).toBe(props.currencySymbol + props.dataList[5]);
     });
 })
