df8334ee713ba07b295a9cdb4d524e991e7bac34
Hayley Krippner <hek21@uclive.ac.nz>
2021-09-28 20:16:20 +1300

    S302T400-854 #time 20m Started making the FeedbackNotification. Read up on how to initialize it in JavaScript to make it active. ?implement

---
+++ frontend/src/components/FeedbackNotification.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div role="alert" aria-live="assertive" aria-atomic="true" class="toast" data-autohide="false">
+      <div class="toast-header">
+        <strong class="mr-auto">{{ this.headingText }}</strong>
+      </div>
+      <div class="toast-body">
+        {{ feedbackText }}
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "FeedbackNotification",
+  props: {
+    isErrorFeedback: {
+      type: Boolean,
+      default: true,
+      required: true
+    },
+    feedbackText: {
+      type: String,
+      default: "",
+      required: true
+    },
+    data() {
+      return {
+        headingText: "",
+      }
+    }
+  },
+  methods: {
+    /**
+     * Determines whether the message is an error message or a success message and sets the heading text accordingly.
+     */
+    setFeedbackStyling() {
+      if(this.isErrorFeedback) {
+        this.headingText = "Error";
+        document.getElementById('toast-header').classList.add('') //TODO
+      }
+    }
+  },
+  /**
+   * Sets the toast's heading upon mounting
+   */
+  mounted() {
+    this.setFeedbackStyling();
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
