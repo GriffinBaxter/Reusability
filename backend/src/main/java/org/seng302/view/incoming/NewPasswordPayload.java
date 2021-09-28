29a303164e9b7ea8946e7d219f9d1e1dcacd559c
Troy <tpt38@uclive.ac.nz>
2021-09-28 13:38:46 +1300

    S302T400-878 #time 5m Added a missing javadoc and updated the postman collection with error status messages ?chore ?test

--- backend/src/main/java/org/seng302/view/incoming/NewPasswordPayload.java
+++ backend/src/main/java/org/seng302/view/incoming/NewPasswordPayload.java
@@ -1,5 +1,8 @@
 package org.seng302.view.incoming;
 
+/**
+ * Class for the new password payload when the user forgets their password.
+ */
 public class NewPasswordPayload {
     private String password;
 
