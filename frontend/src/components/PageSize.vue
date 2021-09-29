a739e386abedc21f8d62963cf84efcc15d71c236
Zachary Kaye <zck10@uclive.ac.nz>
2021-09-29 13:47:26 +1300

    S302T400-869 #time 30m Created the PageSize component. ?implement
    - Updated the API spec to make token a query for PUT /users/forgotPassword
    - Made the base layout of the PageSize component and tried to implement it on the Marketplace, but I was having difficulty with styling.

---
+++ frontend/src/components/PageSize.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="float-end">
+
+  <label for="page-size-button" class="py-3">
+    Page Size:
+  </label>
+
+  <div class="btn-group col d-inline-block p-2" role="group">
+
+    <button type="button" class="btn green-button dropdown-toggle order-by-options-btn w-100"
+            id="page-size-button"
+            data-bs-toggle="dropdown" aria-expanded="false">{{ selectedPageSize }}
+    </button>
+
+    <ul class="dropdown-menu gap-2" aria-labelledby="btnGroupDrop1">
+      <li>
+        <div class="btn green-button-transparent col-12 order-by-options-btn"
+             v-for="size in pageSizes" v-bind:key="size"
+             @click="selectedPageSize = size; emitPageSize()">
+          {{ size }}
+        </div>
+      </li>
+    </ul>
+
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageSize",
+  props: {
+    pageSizes: {
+      type: Array,
+      default: () => [5, 10, 15],
+      required: true
+    }
+  },
+  data()  {
+    return {
+      // the page size the user has selected.
+      selectedPageSize: "",
+    }
+  },
+  methods: {
+    /**
+     * When a user has selected a page size this size should be emitted so that other components/pages can
+     * adjust their page sizes accordingly.
+     */
+    emitPageSize() {
+      this.$emit('selectedPageSize', this.selectedPageSize);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
